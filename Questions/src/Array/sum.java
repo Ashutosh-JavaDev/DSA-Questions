24451613d8f5a6dd427d571e2954d37db3d3b91b
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-16 17:28:29 +0530

    Practice Questions

--- Questions/src/Array/sum.java
+++ Questions/src/Array/sum.java
@@ -1,13 +1,17 @@
 package Array;
+
 import java.util.Scanner;
-class Sum{
-    int arr[],size;
-     void result(){
-        Scanner sc=new Scanner(System.in);
+
+class Sum {
+    int arr[], size;
+
+    void result() {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the Array");
-        size=sc.nextInt();
+        size = sc.nextInt();
     }
 }
+
 public class sum {
-    
+
 }
