dbedf1bd022249447cd409c3aa95583c53e7c22f
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-16 17:28:12 +0530

    Practice Questions

--- Questions/src/Array/sum.java
+++ Questions/src/Array/sum.java
@@ -4,6 +4,8 @@ class Sum{
     int arr[],size;
      void result(){
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the Array");
+        size=sc.nextInt();
     }
 }
 public class sum {
