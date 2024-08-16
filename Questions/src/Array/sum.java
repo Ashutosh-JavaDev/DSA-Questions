2aa5ec7a411b0941794f5961f3a91909f0c540e1
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-16 17:29:41 +0530

    Practice Questions

--- Questions/src/Array/sum.java
+++ Questions/src/Array/sum.java
@@ -9,6 +9,11 @@ class Sum {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the Array");
         size = sc.nextInt();
+        arr=new int[size];
+        System.out.println("Enter the Value in the Array");
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
     }
 }
 
