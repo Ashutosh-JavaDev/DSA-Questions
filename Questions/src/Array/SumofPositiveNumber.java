6c7dbf0d60eef7b33f401e96dbf82323b017e9b9
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-17 23:38:23 +0530

    Practice Questions

--- Questions/src/Array/SumofPositiveNumber.java
+++ Questions/src/Array/SumofPositiveNumber.java
@@ -1,5 +1,10 @@
 package Array;
 
 public class SumofPositiveNumber {
-    
+    int arr[],size;
+    SumofPositiveNumber(){
+        BasicClass ob=new BasicClass();
+        this.arr=ob.arr;
+        this.size=ob.size;
+    }
 }
