0ef0a22b3dc137f9750fecc954713a228091dc6c
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-17 23:04:30 +0530

    Practice Questions

--- Questions/src/Array/Positive.java
+++ Questions/src/Array/Positive.java
@@ -9,6 +9,10 @@ public class Positive {
         this.arr = arr;
         this.size = size;
         new BasicClass(arr, size);
-
+        for(int i=0;i<size;i++){
+            if(arr[i]>0){
+                System.out.print(arr[i]+" ");
+            }
+        }
     }
 }
