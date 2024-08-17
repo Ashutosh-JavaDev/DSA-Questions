402953b9385cc4e3e82dd1fcc61fa1be65ed269b
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-17 22:38:23 +0530

    Practice Questions

--- Questions/src/Array/Positive.java
+++ Questions/src/Array/Positive.java
@@ -1,7 +1,12 @@
 package Array;
 import java.util.Scanner;
 public class Positive {
+    int arr[],size;
     Positive(){
-
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Size of the Array");
+        size=sc.nextInt();
+        arr=new int[size]; 
+        System.out.println("Enter the Value in the Array");
     }
 }
