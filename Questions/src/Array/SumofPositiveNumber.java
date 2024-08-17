67d9512f1b8ad67c28de1cfb22d2d08e2873a2b9
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-17 23:41:04 +0530

    Practice Questions

--- Questions/src/Array/SumofPositiveNumber.java
+++ Questions/src/Array/SumofPositiveNumber.java
@@ -1,16 +1,17 @@
 package Array;
 
 public class SumofPositiveNumber {
-    int arr[],size;
-    SumofPositiveNumber(){
-        BasicClass ob=new BasicClass();
-        this.arr=ob.arr;
-        this.size=ob.size;
-        int sum=0;
-        System.out.print("Sum of "+size+" Positive Number is: ");
-        for(int i=0;i<size;i++){
-            if(arr[i]>0){
-                sum+=arr[i];
+    int arr[], size;
+
+    SumofPositiveNumber() {
+        BasicClass ob = new BasicClass();
+        this.arr = ob.arr;
+        this.size = ob.size;
+        int sum = 0;
+        System.out.print("Sum of " + size + " Positive Number is: ");
+        for (int i = 0; i < size; i++) {
+            if (arr[i] > 0) {
+                sum += arr[i];
             }
         }
 
