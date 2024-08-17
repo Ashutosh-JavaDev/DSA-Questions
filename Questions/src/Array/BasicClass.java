2940151f70a49ed8a0103df4709ba975969ab1a1
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-17 22:44:52 +0530

    Practice Questions

--- Questions/src/Array/BasicClass.java
+++ Questions/src/Array/BasicClass.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 
 public class BasicClass {
     class basic {
+        int arr[],size;
         public void Basic() {
             Scanner sc = new Scanner(System.in);
             System.out.println("Enter the Size of the Array");
-            int size = sc.nextInt();
-            int arr[] = new int[size];
+             size = sc.nextInt();
+             arr = new int[size];
             System.out.println("Enter the Value in the Array");
             for (int i = 0; i < size; i++) {
                 arr[i] = sc.nextInt();
--- Questions/src/Array/Positive.java
+++ Questions/src/Array/Positive.java
@@ -5,14 +5,7 @@ import java.util.Scanner;
 public class Positive {
 
     Positive() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Size of the Array");
-        int size = sc.nextInt();
-        int arr[] = new int[size];
-        System.out.println("Enter the Value in the Array");
-        for (int i = 0; i < size; i++) {
-            arr[i] = sc.nextInt();
-        }
+       new BasicClass();
 
     }
 }
