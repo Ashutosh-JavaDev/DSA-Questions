f331e1905b2462b9afab86f237df78f02fdee50e
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-17 23:03:11 +0530

    Practice Questions

--- Questions/src/Array/Positive.java
+++ Questions/src/Array/Positive.java
@@ -3,9 +3,11 @@ package Array;
 import java.util.Scanner;
 
 public class Positive {
-
+    int arr[],size;
     Positive() {
+        this.arr=arr;
+        this.size=size;
        new BasicClass();
-        
+
     }
 }
