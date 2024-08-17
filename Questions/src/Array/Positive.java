8ee901999ac29549f6e6f637cd102b5d38769c3d
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-17 22:41:04 +0530

    Practice Questions

--- Questions/src/Array/Positive.java
+++ Questions/src/Array/Positive.java
@@ -7,12 +7,12 @@ public class Positive {
     Positive() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Size of the Array");
-       int size = sc.nextInt();
-       int arr[] = new int [size];
+        int size = sc.nextInt();
+        int arr[] = new int[size];
         System.out.println("Enter the Value in the Array");
         for (int i = 0; i < size; i++) {
             arr[i] = sc.nextInt();
         }
-        
+
     }
 }
