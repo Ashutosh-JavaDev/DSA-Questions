5581a4d1cba500a8a487aa770645c841840d4e5d
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-17 22:42:15 +0530

    Practice Questions

---
+++ Questions/src/Array/BasicClass.java
@@ -0,0 +1,16 @@
+package Array;
+import java.util.Scanner;
+public class BasicClass {
+    class basic{
+        public void Basic(){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the Size of the Array");
+            int size = sc.nextInt();
+            int arr[] = new int[size];
+            System.out.println("Enter the Value in the Array");
+            for (int i = 0; i < size; i++) {
+                arr[i] = sc.nextInt();
+            }
+        }
+    }
+}
