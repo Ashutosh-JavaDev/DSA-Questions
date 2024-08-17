446eb89230695d9bc8ce0e8675a78b40fab527f4
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-17 23:40:58 +0530

    Practice Questions

--- Questions/src/Array/SumofPositiveNumber.java
+++ Questions/src/Array/SumofPositiveNumber.java
@@ -6,5 +6,14 @@ public class SumofPositiveNumber {
         BasicClass ob=new BasicClass();
         this.arr=ob.arr;
         this.size=ob.size;
+        int sum=0;
+        System.out.print("Sum of "+size+" Positive Number is: ");
+        for(int i=0;i<size;i++){
+            if(arr[i]>0){
+                sum+=arr[i];
+            }
+        }
+
+        System.out.print(sum);
     }
 }
