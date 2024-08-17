2f2771e65bcb1c5d4ff6400ac36515f10359c54f
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-17 23:18:38 +0530

    Practice Questions

--- Questions/src/Array/Positive.java
+++ Questions/src/Array/Positive.java
@@ -9,6 +9,7 @@ public class Positive {
         BasicClass ob=new BasicClass();
         this.arr = ob.getArr();
         this.size = ob.getSize();
+        System.out.print("All Positive Numbers: ");
         for (int i = 0; i < size; i++) {
             if (arr[i] > 0) {
                 System.out.print(arr[i] + " ");
