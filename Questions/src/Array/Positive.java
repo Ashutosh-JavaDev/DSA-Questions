779baabc7d05a3944149fdd4af23ba1a6aef1cb2
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-17 22:38:58 +0530

    Practice Questions

--- Questions/src/Array/Positive.java
+++ Questions/src/Array/Positive.java
@@ -1,15 +1,18 @@
 package Array;
+
 import java.util.Scanner;
+
 public class Positive {
-    int arr[],size;
-    Positive(){
-        Scanner sc=new Scanner(System.in);
+    int arr[], size;
+
+    Positive() {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Size of the Array");
-        size=sc.nextInt();
-        arr=new int[size]; 
+        size = sc.nextInt();
+        arr = new int[size];
         System.out.println("Enter the Value in the Array");
-        for(int i=0;i<size;i++){
-            arr[i]=sc.nextInt();
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
         }
     }
 }
