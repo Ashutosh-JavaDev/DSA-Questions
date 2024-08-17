6f2b2da16740866dc72cfff6332a276419427313
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-17 23:08:31 +0530

    Practice Questions

--- Questions/src/Array/Positive.java
+++ Questions/src/Array/Positive.java
@@ -1,11 +1,13 @@
 package Array;
+
 public class Positive {
     int arr[], size;
 
     Positive() {
+
+        new BasicClass(arr, size);
         this.arr = arr;
         this.size = size;
-        new BasicClass(arr, size);
         for (int i = 0; i < size; i++) {
             if (arr[i] > 0) {
                 System.out.print(arr[i] + " ");
