213c7c574e5d7eb3608c51ae769ea99af434cfd8
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-18 23:10:57 +0530

    Practice Questions

--- Questions/src/Array/SumofNegativeNumber.java
+++ Questions/src/Array/SumofNegativeNumber.java
@@ -1,22 +1,23 @@
 package Array;
 
 public class SumofNegativeNumber {
-    int arr[],size;
-    SumofNegativeNumber(){
-        BasicClass on=new BasicClass();
-        this.arr=on.getArr();
-        this.size=on.getSize();
-        int sum=0;
-        System.out.print("Sum of "+size+" Positive Number is: ");
-        for(int i=0;i<size;i++){
-            if(arr[i]<0){
-                sum+=arr[i];
+    int arr[], size;
+
+    SumofNegativeNumber() {
+        BasicClass on = new BasicClass();
+        this.arr = on.getArr();
+        this.size = on.getSize();
+        int sum = 0;
+        System.out.print("Sum of " + size + " Positive Number is: ");
+        for (int i = 0; i < size; i++) {
+            if (arr[i] < 0) {
+                sum += arr[i];
             }
         }
         System.out.println();
     }
-    public static void main(String[]args){
-    new SumofNegativeNumber();
+
+    public static void main(String[] args) {
+        new SumofNegativeNumber();
     }
 }
-
