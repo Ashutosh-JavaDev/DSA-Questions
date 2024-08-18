29c1122d1705960b7c22692e883ee6cab637ee2a
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-18 23:02:27 +0530

    Practice Questions

--- Questions/src/Array/Negative.java
+++ Questions/src/Array/Negative.java
@@ -7,7 +7,7 @@ public class Negative {
         BasicClass ob = new BasicClass();
         this.arr=ob.getArr();
         this.size=ob.getSize();
-        System.out.print("Negative Number are: ");
+        System.out.print("Negative Number are: "+" ");
         for(int i=0;i<size;i++){
             if(arr[i]<0){
                 System.out.print(arr[i]);
