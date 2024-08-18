2c57b398a1413313526711020ab1682ab1d61330
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-18 23:07:33 +0530

    Practice Questions

--- Questions/src/Array/SumofNegativeNumber.java
+++ Questions/src/Array/SumofNegativeNumber.java
@@ -3,6 +3,9 @@ package Array;
 public class SumofNegativeNumber {
     int arr[],size;
     SumofNegativeNumber(){
-
+        BasicClass on=new BasicClass();
+        this.arr=on.getArr();
+        this.size=on.getSize();
+        
     }
 }
