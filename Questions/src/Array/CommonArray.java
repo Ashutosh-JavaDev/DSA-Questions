5fd3382d97ea8f43ebb4c4eeacb2052f287e525e
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-19 23:46:43 +0530

    Practice Questions

--- Questions/src/Array/CommonArray.java
+++ Questions/src/Array/CommonArray.java
@@ -6,5 +6,12 @@ public class CommonArray {
         BasicClass ob = new BasicClass();
         this.arr=ob.getArr();
         this.size=ob.getSize();
+        for(int i=0;i<size;i++){
+            for(int j=i+1;j<size;j++){
+                if(arr[i]==arr[j]){
+                    
+                }
+            }
+        }
     }
 }
