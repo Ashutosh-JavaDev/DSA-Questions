0330f4be5fb07598463d3a3e840279b857642dcd
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-19 23:52:39 +0530

    Practice Questions

--- Questions/src/Array/CommonArray.java
+++ Questions/src/Array/CommonArray.java
@@ -8,18 +8,20 @@ public class CommonArray {
         this.arr = ob.getArr();
         this.size = ob.getSize();
         int count = 0;
+        int num=0;
         System.out.print("Common Number in the Array: ");
         for (int i = 0; i < size; i++) {
             for (int j = i + 1; j < size; j++) {
                 if (arr[i] == arr[j]) {
                     count++;
+                    num=arr[j];
                 }
-
-            }
-            if (count >= (size % 2)) {
-                System.out.print(arr[i]);
             }
         }
+         if(count>=(size%2)){
+            System.out.print(num);    
+        }
+        System.out.println();
 
     }
     public static void main(String[] args) {
