bc8b9a133889e3afdadc584963ae63bf7f74d4b5
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-20 14:46:30 +0530

    Practice Questions

--- Questions/src/Array/TwoDArray.java
+++ Questions/src/Array/TwoDArray.java
@@ -7,5 +7,12 @@ public class TwoDArray {
         this.arr=ob.arr();
         this.row=ob.getRow();
         this.col=ob.getCol();
+        System.out.println("Two D Array");
+        for(int i=0;i<row;i++){
+            for(int j=0;i<col;j++){
+                System.out.print(arr[i]+" "+arr[j]);
+            }
+        }
+        System.out.println();
     }
 }
