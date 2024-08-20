f42cd29419ca976dce791b08c32755d3803a0e98
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-20 15:22:58 +0530

    Practice Questions

--- Questions/src/Array/CompareTwoMatrics.java
+++ Questions/src/Array/CompareTwoMatrics.java
@@ -12,19 +12,19 @@ public class CompareTwoMatrics {
         System.out.println("Enter the value in Second Matrics");
         TwoDClass ob2=new TwoDClass();
         this.arr2 = ob2.arr();
-
+        boolean areEqual=true;
         for (int i = 0; i < row; i++) {
             for(int j=0;j<col;j++){
-                if(arr1[i][j]==arr2[i][j]){
-                    count++;
-                    roll=col;
+                if(arr1[i][j]!=arr2[i][j]){
+                    areEqual=false;
+                    break;
                 }
-                else{
-                    noncount++;
-                }
+               if(!areEqual){
+                break;
+               }
             }
         }
-        if(roll==(col-1)){
+        if(areEqual){
             System.out.println("Both Matrics are same");
         }
         else{
