624dda778a198d8698b0cc6735294a197f6ec3bd
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-20 14:40:00 +0530

    Practice Questions

--- Questions/src/Array/TwoDClass.java
+++ Questions/src/Array/TwoDClass.java
@@ -1,7 +1,15 @@
 package Array;
-
+import java.util.Scanner;
 public class TwoDClass {
+    int arr1[],arr2[],row,col,size;
     TwoDClass(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of Row");
+        row=sc.nextInt();
+        arr1=new int[row];
+        System.out.println("Enter the size of Column");
+        col=sc.nextInt();
+        arr2=new int[col];
 
     }
 }
