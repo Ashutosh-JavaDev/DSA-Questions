4e24a570a76224fe0f11a12a7fb7cb2554324af1
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-20 14:53:41 +0530

    Practice Questions

--- Questions/src/Array/TwoDArray.java
+++ Questions/src/Array/TwoDArray.java
@@ -1,21 +1,26 @@
 package Array;
 
+import java.io.IOException;
+
 public class TwoDArray {
-    int arr[][],row,col;
-    TwoDArray(){
-        TwoDClass ob=new TwoDClass();
-        this.arr=ob.arr();
-        this.row=ob.getRow();
-        this.col=ob.getCol();
+    int arr[][], row, col;
+
+    TwoDArray() {
+        TwoDClass ob = new TwoDClass();
+        this.arr = ob.arr();
+        this.row = ob.getRow();
+        this.col = ob.getCol();
         System.out.println("Two D Array");
-        for(int i=0;i<row;i++){
-            for(int j=0;i<col;j++){
-                System.out.print(arr[i][j]+" ");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(arr[i][j] + " ");
             }
-        }
         System.out.println();
+
+        }
     }
-    public static  void main(String[]args){
+
+    public static void main(String[] args) throws IOException {
         new TwoDArray();
     }
 }
