7038bb0cfdd420b8ba8ab93aac915b9725105b65
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-20 14:45:23 +0530

    Practice Questions

--- Questions/src/Array/TwoDArray.java
+++ Questions/src/Array/TwoDArray.java
@@ -1,7 +1,11 @@
 package Array;
 
 public class TwoDArray {
+    int arr[][],row,col;
     TwoDArray(){
-        
+        TwoDClass ob=new TwoDClass();
+        this.arr=ob.arr();
+        this.row=ob.getRow();
+        this.col=ob.getCol();
     }
 }
