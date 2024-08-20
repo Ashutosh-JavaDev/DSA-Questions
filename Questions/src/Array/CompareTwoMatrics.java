bfe0cd12676116f9ba230475e343243c87bd12dc
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-20 15:16:01 +0530

    Practice Questions

--- Questions/src/Array/CompareTwoMatrics.java
+++ Questions/src/Array/CompareTwoMatrics.java
@@ -10,10 +10,14 @@ public class CompareTwoMatrics {
         this.arr2 = ob.arr();
         this.row = ob.getRow();
         this.col = ob.getCol();
+        int count=0,noncount=0;
         for (int i = 0; i < row; i++) {
             for(int j=0;j<col;j++){
                 if(arr1[i][j]==arr2[i][j]){
-                    
+                    count++;
+                }
+                else{
+                    noncount++;
                 }
             }
         }
