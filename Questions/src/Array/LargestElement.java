f30eed51687024d969cb10085f017da8e5160051
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-20 14:21:33 +0530

    Practice Questions

--- Questions/src/Array/LargestElement.java
+++ Questions/src/Array/LargestElement.java
@@ -1,20 +1,24 @@
 package Array;
 
 public class LargestElement {
-    int arr[],size;
-  int LargestElement(){
-        BasicClass ob=new BasicClass();
-        this.arr=arr;
-        this.size=size;
-        int max=arr[0];
-        for(int i=0;i<size;i++){
-            if(arr[0]>max){
-                max=arr[i];
+    int arr[], size;
+
+    int LargestElement() {
+        BasicClass ob = new BasicClass();
+        this.arr = ob.getArr();
+        this.size = ob.getSize();
+        int max = arr[0];
+        for (int i = 0; i < size; i++) {
+            if (arr[0] > max) {
+                max = arr[i];
             }
         }
         return max;
     }
-    public static void main(String[]args){
-        new LargestElement();
+
+    public static void main(String[] args) {
+       LargestElement ob= new LargestElement();
+       int result=ob.LargestElement();
+       System.out.println("Maximum Number in the Array: "+result);
     }
 }
