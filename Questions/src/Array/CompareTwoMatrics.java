db92559d9f1a02a985eab09def78859946ac3ebd
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-20 15:03:41 +0530

    Practice Questions

--- Questions/src/Array/CompareTwoMatrics.java
+++ Questions/src/Array/CompareTwoMatrics.java
@@ -1,10 +1,12 @@
 package Array;
 
 public class CompareTwoMatrics {
-    int arr1[],arr2[],row,col;
+    int arr1[][],arr2[][],row,col;
     CompareTwoMatrics(){
 
     TwoDClass ob=new TwoDClass();
+        this.arr1=ob.arr();
+        this.arr2=ob.arr();
 
     }
 
