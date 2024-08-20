e01b3988705c2c76e7384fc6ea75d3d559c85265
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-20 14:42:07 +0530

    Practice Questions

--- Questions/src/Array/TwoDClass.java
+++ Questions/src/Array/TwoDClass.java
@@ -1,15 +1,19 @@
 package Array;
 import java.util.Scanner;
 public class TwoDClass {
-    int arr1[],arr2[],row,col,size;
+    int arr[][],row,col,size;
     TwoDClass(){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the size of Row");
         row=sc.nextInt();
-        arr1=new int[row];
         System.out.println("Enter the size of Column");
         col=sc.nextInt();
-        arr2=new int[col];
-
+        arr=new int[row][col];
+        System.out.println("Enter the value in the Matrix");
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
     }
 }
