6cc62fd8ab57ed2381f3be2b6daeddd3f77561cd
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-21 21:03:41 +0530

    Practice Questions

--- Questions/src/Array/Search.java
+++ Questions/src/Array/Search.java
@@ -1,7 +1,10 @@
 package Array;
 
 public class Search {
+    int arr[],size;
     Search(){
         BasicClass ob=new BasicClass();
+        this.arr=ob.getArr();
+        this.size=ob.getSize();
     }
 }
