68cc218fcea28fdbd225efdb1c585f9a8d3a1c3d
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-21 21:16:15 +0530

    Practice Questions

--- Questions/src/Array/Search.java
+++ Questions/src/Array/Search.java
@@ -1,5 +1,7 @@
 package Array;
+
 import java.util.Scanner;
+
 public class Search {
     int arr[], size;
 
@@ -7,20 +9,19 @@ public class Search {
         BasicClass ob = new BasicClass();
         this.arr = ob.getArr();
         this.size = ob.getSize();
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Number wants to be Search");
-        int num=sc.nextInt();
-        int  count=0;
-        for(int i=0;i<size;i++){
-            if(arr[i]==num){
+        int num = sc.nextInt();
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == num) {
                 count++;
-                System.out.println("Position["+i+"]");
+                System.out.println("Position[" + i + "]");
             }
         }
-        if(count>0){
+        if (count > 0) {
             System.out.println("Number Found");
-        }
-        else{
+        } else {
             System.out.println("Number Not Found");
         }
     }
