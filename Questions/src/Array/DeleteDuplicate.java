78ad6857f1b744cc9c0a2e54e4ca695540dd08aa
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-23 00:19:51 +0530

    Practice Questions

--- Questions/src/Array/DeleteDuplicate.java
+++ Questions/src/Array/DeleteDuplicate.java
@@ -12,6 +12,7 @@ public class DeleteDuplicate {
         for (int i = 0; i < size; i++) {
             for (int j = i + 1; j < size; j++) {
                 if (arr[i] == arr[j]) {
+                    temp[i]=arr[i];
                     count++;
 
                 }
