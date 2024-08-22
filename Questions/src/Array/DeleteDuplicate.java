fa632780e61ac2096b8a6cb52a678c116a1bbef0
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-22 23:54:11 +0530

    Practice Questions

--- Questions/src/Array/DeleteDuplicate.java
+++ Questions/src/Array/DeleteDuplicate.java
@@ -7,18 +7,17 @@ public class DeleteDuplicate {
         BasicClass ob = new BasicClass();
         this.arr = ob.getArr();
         this.size = ob.getSize();
-        int count=0;
-        for(int i=0;i<size;i++){
-            for(int j=i+1;j<size;j++){
-                if(arr[i]==arr[j]){
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if (arr[i] == arr[j]) {
                     count++;
                 }
             }
         }
-        if(count>0){
-            System.out.println("Total Number of Duplicate found in array: "+count);
-        }
-        else{
+        if (count > 0) {
+            System.out.println("Total Number of Duplicate found in array: " + count);
+        } else {
             System.out.println("No Duplicates Found");
         }
     }
