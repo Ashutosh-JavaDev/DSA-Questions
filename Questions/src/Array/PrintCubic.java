c2d0fe94f0ed8230df6ea4969de618eb5b34f172
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-23 22:38:27 +0530

    Practice Questions

--- Questions/src/Array/PrintCubic.java
+++ Questions/src/Array/PrintCubic.java
@@ -3,8 +3,13 @@ package Array;
 public class PrintCubic {
     int arr[],size;
     PrintCubic(){
+        int num=0;
         BasicClass ob=new BasicClass();
         this.arr=ob.getArr();
         this.size=ob.getSize();
+        for(int i=0;i<size;i++){
+            num=arr[i]*arr[i]*arr[i];
+        }
+        
     }
 }
