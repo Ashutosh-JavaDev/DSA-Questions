472c8ef89de89e57bea25f5b4bae9884d87ec2bc
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-25 20:12:35 +0530

    Practice Questions

--- Questions/src/Array/CopyArray.java
+++ Questions/src/Array/CopyArray.java
@@ -13,7 +13,7 @@ public class CopyArray {
         }
         System.out.println("Copied array from Array");
         for (int i = 0; i < size; i++) {
-
+            System.out.print(copy[i]+" ");
         }
     }
 }
