d8f726431e2e7f569005ad7cee4089b00dc00e13
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-25 20:19:43 +0530

    Practice Questions

--- Questions/src/Array/StoreEven.java
+++ Questions/src/Array/StoreEven.java
@@ -1,14 +1,14 @@
 package Array;
 
 public class StoreEven {
-    int arr[], second[], third[],size;
+    int arr[], second[], third[], size;
 
     StoreEven() {
         BasicClass ob = new BasicClass();
         this.arr = ob.getArr();
         this.size = ob.getSize();
-        BasicClass ob2=new BasicClass();
-        this.arr=ob2.getArr();
-        this.size=ob2.getSize();
+        BasicClass ob2 = new BasicClass();
+        this.arr = ob2.getArr();
+        this.size = ob2.getSize();
     }
 }
