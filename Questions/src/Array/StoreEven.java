7f9a1564e11a43309481197675a334c4f0c7f441
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-26 23:28:58 +0530

    Practice Questions

--- Questions/src/Array/StoreEven.java
+++ Questions/src/Array/StoreEven.java
@@ -1,31 +1,35 @@
 package Array;
 
 public class StoreEven {
-    int arr[], second[], third[], size;
+    int arr[], second[], third[], size1, size2;
 
     StoreEven() {
         BasicClass ob = new BasicClass();
         this.arr = ob.getArr();
-        this.size = ob.getSize();
+        this.size1 = ob.getSize();
         BasicClass ob2 = new BasicClass();
         this.second = ob2.getArr();
-        this.size = ob2.getSize();
+        this.size2 = ob2.getSize();
         int even1 = 0;
-        int even2=0;
-        int total=even1+even2;
+        int even2 = 0;
+        int total = even1 + even2;
         third = new int[total];
-        for(int i=0;i<size;i++){
-            if(arr[i]%2==0){
+        for (int i = 0; i < size1; i++) {
+            if (arr[i] % 2 == 0) {
                 even1++;
             }
         }
-        for(int i=0;i<size;i++){
-            if(second[i]%2==0){
+        for (int i = 0; i < size2; i++) {
+            if (second[i] % 2 == 0) {
                 even2++;
             }
         }
-        for(int i=0;i<total;i++){
-            third[i]=arr[i];
+        int index = 0;
+        for (int i = 0; i < size1; i++) {
+            if (arr[i] % 2 == 0) {
+                third[index++] = arr[i];
+
+            }
         }
     }
 }
