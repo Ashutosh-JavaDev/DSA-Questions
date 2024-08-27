6d0125cf9779d2e06f8453f9dfca7321f5b7c4b5
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-27 23:15:55 +0530

    Practice Questions

--- Questions/src/Array/DiagonalMatrix.java
+++ Questions/src/Array/DiagonalMatrix.java
@@ -1,11 +1,12 @@
 package Array;
 
 public class DiagonalMatrix {
-    int arr1[][],row,col;
-    DiagonalMatrix(){
-        TwoDClass ob=new TwoDClass();
-        this.arr1=ob.arr();
-        this.row=ob.getRow();
-        this.col=ob.getCol();
+    int arr1[][], row, col;
+
+    DiagonalMatrix() {
+        TwoDClass ob = new TwoDClass();
+        this.arr1 = ob.arr();
+        this.row = ob.getRow();
+        this.col = ob.getCol();
     }
 }
