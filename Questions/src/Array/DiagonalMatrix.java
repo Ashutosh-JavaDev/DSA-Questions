c6d48162f87e2ab0c197592ef2d89abc659d4e3f
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-27 23:18:00 +0530

    Practice Questions

--- Questions/src/Array/DiagonalMatrix.java
+++ Questions/src/Array/DiagonalMatrix.java
@@ -1,17 +1,19 @@
 package Array;
 
 public class DiagonalMatrix {
-    int arr1[][], row, col;
+    int arr[][], row, col;
 
     DiagonalMatrix() {
         TwoDClass ob = new TwoDClass();
-        this.arr1 = ob.arr();
+        this.arr = ob.arr();
         this.row = ob.getRow();
         this.col = ob.getCol();
         int sum = 0;
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-
+                if(i==j){
+                    sum+=arr[i][j];
+                }
             }
         }
     }
