baa1174efc56752f41808f876f886401e8a72ef8
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-27 23:23:15 +0530

    Practice Questions

--- Questions/src/Array/DiagonalMatrix.java
+++ Questions/src/Array/DiagonalMatrix.java
@@ -16,6 +16,7 @@ public class DiagonalMatrix {
             System.out.println();
 
         }
+        System.out.println();
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 if (i == j) {
@@ -25,6 +26,7 @@ public class DiagonalMatrix {
                 }
             }
         }
+        System.out.println();
         System.out.println("Sum of Diagonal Matrix: " + sum);
     }
 
