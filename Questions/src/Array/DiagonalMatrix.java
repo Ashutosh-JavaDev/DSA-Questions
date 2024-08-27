03b1f155e2be133d05f684279744ab5f9c335d2e
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-27 23:17:10 +0530

    Practice Questions

--- Questions/src/Array/DiagonalMatrix.java
+++ Questions/src/Array/DiagonalMatrix.java
@@ -8,5 +8,11 @@ public class DiagonalMatrix {
         this.arr1 = ob.arr();
         this.row = ob.getRow();
         this.col = ob.getCol();
+        int sum = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+
+            }
+        }
     }
 }
