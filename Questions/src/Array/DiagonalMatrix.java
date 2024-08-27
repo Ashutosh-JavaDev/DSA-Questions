9703ad96a00f1ef7ea49036c5f4a9bcbfec33838
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-27 23:19:29 +0530

    Practice Questions

--- Questions/src/Array/DiagonalMatrix.java
+++ Questions/src/Array/DiagonalMatrix.java
@@ -11,10 +11,18 @@ public class DiagonalMatrix {
         int sum = 0;
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-                if(i==j){
-                    sum+=arr[i][j];
+                System.out.print(arr[i][j] + " ");
+            }
+
+        }
+        System.out.println();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (i == j) {
+                    sum += arr[i][j];
                 }
             }
         }
+
     }
 }
