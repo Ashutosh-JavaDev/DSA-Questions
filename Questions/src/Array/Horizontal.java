173fba501b188c607158a78e7d4534c7a81d7aa8
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-28 23:51:39 +0530

    Practice Questions

--- Questions/src/Array/Horizontal.java
+++ Questions/src/Array/Horizontal.java
@@ -4,7 +4,9 @@ public class Horizontal {
     int arr[][], row, col;
 
     Horizontal() {
-        TwoDClass ob=new TwoDClass();
-        
+        TwoDClass ob = new TwoDClass();
+        this.arr = ob.arr();
+        this.row = ob.getRow();
+        this.col = ob.getCol();
     }
 }
