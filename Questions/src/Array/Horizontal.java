059659b3242d461c983c7ad4837b42781a0ae37f
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-28 23:53:26 +0530

    Practice Questions

--- Questions/src/Array/Horizontal.java
+++ Questions/src/Array/Horizontal.java
@@ -8,5 +8,10 @@ public class Horizontal {
         this.arr = ob.arr();
         this.row = ob.getRow();
         this.col = ob.getCol();
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                
+            }
+        }
     }
 }
