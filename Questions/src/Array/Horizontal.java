94d071c2119c56c21f4925fc23d8ff32356b0992
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-28 23:56:38 +0530

    Practice Questions

--- Questions/src/Array/Horizontal.java
+++ Questions/src/Array/Horizontal.java
@@ -8,6 +8,12 @@ public class Horizontal {
         this.arr = ob.arr();
         this.row = ob.getRow();
         this.col = ob.getCol();
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
         int sum=0;
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
