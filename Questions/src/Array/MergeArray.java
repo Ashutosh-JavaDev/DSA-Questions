668d18b74172396e50cb27005699a44249aee61d
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-29 22:00:09 +0530

    Practice Questions

--- Questions/src/Array/MergeArray.java
+++ Questions/src/Array/MergeArray.java
@@ -1,12 +1,13 @@
 package Array;
 
 public class MergeArray {
-    int arr1[][],row,col,arr2[][];
-    MergeArray(){
-        TwoDClass ob=new TwoDClass();
-        this.arr1=ob.arr();
-        this.row=ob.getRow();
-        this.col=ob.getCol();
+    int arr1[][], row, col, arr2[][];
+
+    MergeArray() {
+        TwoDClass ob = new TwoDClass();
+        this.arr1 = ob.arr();
+        this.row = ob.getRow();
+        this.col = ob.getCol();
 
     }
 }
