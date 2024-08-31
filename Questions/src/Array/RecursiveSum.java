c8bb66e899a92be6e457a93a3ae36ebaab9d01b7
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-31 23:41:04 +0530

    Practice Questions

--- Questions/src/Array/RecursiveSum.java
+++ Questions/src/Array/RecursiveSum.java
@@ -2,14 +2,19 @@ package Array;
 
 public class RecursiveSum {
     // int arr[],size;
-    int sum=0,size=1;
-    public void recursiveSum(int size){
+    // ,size=1;
+    public int recursiveSum(int size){
+        int sum=0;
         if(size==5){
-            return;
+            return 0;
         }
         else{
             sum+=size+1;
            recursiveSum(size+1); 
         }
+        return sum;
+    }
+    public static void main(String[]args){
+        new RecursiveSum();
     }
 }
