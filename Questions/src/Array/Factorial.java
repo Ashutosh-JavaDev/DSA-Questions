d6608c9734225928f6e364060a7485a30dcf0470
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-02 23:21:02 +0530

    Practice Questions

--- Questions/src/Array/Factorial.java
+++ Questions/src/Array/Factorial.java
@@ -1,10 +1,13 @@
 package Array;
 
 public class Factorial {
-    
+    int fact=1;
     public int factorial(int size,int num) {
         if(size==num){
             return 0;
+        }
+        else{
+            fact*=size;
         }
     }
 }
