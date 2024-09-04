2edb8ccdbb3d49b2b962a9da35771cc8c04b68d2
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-04 16:07:01 +0530

    Practice Questions

--- Questions/src/Array/SumofDigit.java
+++ Questions/src/Array/SumofDigit.java
@@ -3,9 +3,9 @@ package Array;
 public class SumofDigit {
     public int sumofdigit(int num){
         int n=num,sum=0;
-        while(num>=0){
-            sum+=num/10;
-            num=num%10;
+        while(n>0){
+            sum+=n%10;
+            n=n/10;
         }
         return sum;
     }    
