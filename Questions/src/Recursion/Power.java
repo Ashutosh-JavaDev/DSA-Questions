2a09f1e6daab3d8bc2c198179e3c2b906472db3d
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-05 20:47:10 +0530

    Practice Questions

--- Questions/src/Recursion/Power.java
+++ Questions/src/Recursion/Power.java
@@ -2,12 +2,12 @@ package Recursion;
 public class Power {
     public int power(int number,int pow){
 
-        int result=0;
+        int result=1;
         if(pow<1){
             return 0;
         }
         else{
-            
+            result*=number;
         }
         return result;
     }
