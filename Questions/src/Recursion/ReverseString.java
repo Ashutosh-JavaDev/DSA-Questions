183c47399efc974be601cc2cfb602e50749db497
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-06 22:56:53 +0530

    Practice Questions

--- Questions/src/Recursion/ReverseString.java
+++ Questions/src/Recursion/ReverseString.java
@@ -3,8 +3,10 @@ package Recursion;
 public class ReverseString {
     String str;
 
-    public String reverseString() {
-        
+    public String reverseString(String st) {
+        if(st.length()<1){
+        return 0;
+        }
         return str;
     }
 }
