421dd99e9c73ca304596947d993c1302bd341583
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-09 21:43:05 +0530

    Practice Questions

--- Questions/src/Recursion/Pallindrome.java
+++ Questions/src/Recursion/Pallindrome.java
@@ -1,20 +1,20 @@
 package Recursion;
 
 public class Pallindrome {
-    int rev=0, d=1;
-    public void check(int num){
-        if(num==0){
+    int rev = 0, d = 1;
+
+    public void check(int num) {
+        if (num == 0) {
             return;
-        }
-        else{
-            rev=num%10;
-            d=d*10+rev;
-            check(num/10);
+        } else {
+            rev = rev * 10 + num % 10;
+            check(num / 10);
         }
     }
-    public static void main(String[]args){
-        Pallindrome  ob=new Pallindrome();
+
+    public static void main(String[] args) {
+        Pallindrome ob = new Pallindrome();
         ob.check(121);
-        
+
     }
 }
