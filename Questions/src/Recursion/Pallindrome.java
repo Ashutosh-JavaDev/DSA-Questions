9aa915e5e6be65fad7c3551040879f47ab5939bd
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-09 21:29:43 +0530

    Practice Questions

--- Questions/src/Recursion/Pallindrome.java
+++ Questions/src/Recursion/Pallindrome.java
@@ -1,9 +1,13 @@
 package Recursion;
 
 public class Pallindrome {
+    int rev=0;
     public void check(int num){
         if(num==0){
             return;
+        }
+        else{
+            rev=num/10;
         }
     }
 }
