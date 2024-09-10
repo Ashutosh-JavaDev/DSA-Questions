887786795c37736740fc357efac8c7c01be74430
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-10 20:31:25 +0530

    Practice Questions

--- Questions/src/Recursion/StringCount.java
+++ Questions/src/Recursion/StringCount.java
@@ -3,6 +3,14 @@ package Recursion;
 public class StringCount {
     int count=0;
     public int result(String str){
+        if(str.length()<0){
+            return 0;
+        }
+        else{
+            // char ch=str.charAt(str.length()-1);
+            count++;
+            result(str.substring(0, str.length()-1));
+        }
         return count;
     }    
 }
