63cca81f001bca39c6883418b98783ac195e03f5
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-10 21:20:30 +0530

    Practice Questions

--- Questions/src/Recursion/SumofNaturalNumber.java
+++ Questions/src/Recursion/SumofNaturalNumber.java
@@ -3,7 +3,13 @@ package Recursion;
 public class SumofNaturalNumber {
     int sum=0;
     public int Result(int size){
-        
+        if(size<1){
+            return 0;
+        }
+        else{
+            sum+=size;
+            Result(size-1);
+        }
         return 0;
     }
 }
