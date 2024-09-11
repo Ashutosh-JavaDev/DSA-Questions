ed9f2ccef00337933a8b4c52667b485885538e0c
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-11 20:14:33 +0530

    Practice Questions

--- Questions/src/Recursion/FindMaximum.java
+++ Questions/src/Recursion/FindMaximum.java
@@ -3,6 +3,12 @@ package Recursion;
 public class FindMaximum {
     int max=0;
     public int maximum(int arr[],int size){
-        
+        if(size==0){
+            return 0;
+        }
+        else{
+
+        }
+        return max;
     }
 }
