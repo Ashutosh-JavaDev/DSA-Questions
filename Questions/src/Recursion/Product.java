2e7620b3632488e1735ae73a53ae1d65f05a7f12
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-11 20:07:42 +0530

    Practice Questions

--- Questions/src/Recursion/Product.java
+++ Questions/src/Recursion/Product.java
@@ -3,7 +3,9 @@ package Recursion;
 public class Product {
     int pro=1;
     public int result(int arr[],int size){
-        
+        if(size==0){
+            return 0;
+        }
         return pro;
     }
 }
