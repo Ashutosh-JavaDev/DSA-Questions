463a9b2f2c0e197fa670406ebfadfe0e64924a57
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-12 20:25:40 +0530

    Practice Questions

--- Questions/src/Recursion/chrckArray.java
+++ Questions/src/Recursion/chrckArray.java
@@ -2,9 +2,13 @@ package Recursion;
 
 public class chrckArray {
     boolean result=false;
+    int sorting=0;
     public boolean check(int arr[],int size){
         if(size==0){
             return result;
+        }
+        else{
+
         }
         return result;
     }
