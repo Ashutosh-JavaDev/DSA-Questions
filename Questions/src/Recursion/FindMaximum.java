9ff5007f97b0ca0091dcc98d9f7aee59329d0f32
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-12 20:09:27 +0530

    Practice Questions

--- Questions/src/Recursion/FindMaximum.java
+++ Questions/src/Recursion/FindMaximum.java
@@ -1,26 +1,27 @@
 package Recursion;
 
 public class FindMaximum {
-    int max=0;
-    public int maximum(int arr[],int size){
-        if(size==0){
+    int max = 0;
+
+    public int maximum(int arr[], int size) {
+        if (size == 0) {
             return 0;
-        }
-        else{
-            if(max>arr[size-1]){
-                max=arr[size-1];
-                maximum(arr, size-1);
+        } else {
+            if (max > arr[size - 1]) {
+                max = arr[size - 1];
+                maximum(arr, size - 1);
             }
-            else{
-                maximum(arr, size-1);
-            }
+
+            maximum(arr, size - 1);
+
         }
         return max;
     }
-    public static void main(String[]args){
-        FindMaximum ob=new FindMaximum();
-        int arr[]={1,3,5,23,2,6};
-        int maxi=ob.maximum(arr, arr.length);
-        System.out.println("Result: "+maxi);
+
+    public static void main(String[] args) {
+        FindMaximum ob = new FindMaximum();
+        int arr[] = { 1, 3, 5, 23, 2, 6 };
+        int maxi = ob.maximum(arr, arr.length);
+        System.out.println("Result: " + maxi);
     }
 }
