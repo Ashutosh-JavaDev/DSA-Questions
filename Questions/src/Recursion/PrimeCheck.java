36ef20fb0fe98b3b86acf53d1bfbe8de50c17b46
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-12 22:55:33 +0530

    Practice Questions

--- Questions/src/Recursion/PrimeCheck.java
+++ Questions/src/Recursion/PrimeCheck.java
@@ -1,9 +1,17 @@
 package Recursion;
 
 public class PrimeCheck {
-    int count =0;
+    int count=0;
     boolean result=true;
-    public boolean check(int num){
+    public boolean check(int num,int counter){
+        if(num==0){
+            return true;
+        }
+        else{
+            if(num%counter==0){
+                count++;
+            }
+        }
         return result;
     }
 }
