cb819930c532d8a5109febe19eeede9a1074ab2a
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-13 22:40:19 +0530

    Practice Questions

--- Questions/src/Recursion/CountVowel.java
+++ Questions/src/Recursion/CountVowel.java
@@ -4,7 +4,7 @@ public class CountVowel {
     int count = 0;
 
     public int check(String str) {
-        if (str.length() < 0) {
+        if (str.length() == 0) {
             return 0;
         }
         if (str.charAt(str.length() - 1) == 'a' || str.charAt(str.length() - 1) == 'e' || str.charAt(str.length() - 1) == 'i'
@@ -13,6 +13,9 @@ public class CountVowel {
                 || str.charAt(str.length() - 1) == 'O' || str.charAt(str.length() - 1) == 'U') {
                     check(str.substring(0, str.length()-1));
                     count++;
+        }
+        else{
+            check(str.substring(0, str.length()-1));
         }
         return count;
     }
