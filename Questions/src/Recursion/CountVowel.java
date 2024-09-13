480bb60e8a1193c19a84ed60a444a66c438ec498
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-13 22:30:24 +0530

    Practice Questions

--- Questions/src/Recursion/CountVowel.java
+++ Questions/src/Recursion/CountVowel.java
@@ -2,7 +2,10 @@ package Recursion;
 
 public class CountVowel {
     int count=0;
-    public int check(int str[]){
+    public int check(int str,int size){
+        if(size<0){
+            return 0;
+        }
         return count;
     }
 }
