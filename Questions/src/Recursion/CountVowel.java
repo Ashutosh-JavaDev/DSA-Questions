0c2ade679e88fe531966cda9a2f8e8a0a9f3c5c1
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-13 22:36:04 +0530

    Practice Questions

--- Questions/src/Recursion/CountVowel.java
+++ Questions/src/Recursion/CountVowel.java
@@ -3,15 +3,16 @@ package Recursion;
 public class CountVowel {
     int count = 0;
 
-    public int check(String str, int size) {
-        if (size < 0) {
+    public int check(String str) {
+        if (str.length() < 0) {
             return 0;
         }
-        if (str.charAt(size - 1) == 'a' || str.charAt(size - 1) == 'e' || str.charAt(size - 1) == 'i'
-                || str.charAt(size - 1) == 'o' || str.charAt(size - 1) == 'u' || str.charAt(size - 1) == 'A'
-                || str.charAt(size - 1) == 'E' || str.charAt(size - 1) == 'I'
-                || str.charAt(size - 1) == 'O' || str.charAt(size - 1) == 'U') {
-
+        if (str.charAt(str.length() - 1) == 'a' || str.charAt(str.length() - 1) == 'e' || str.charAt(str.length() - 1) == 'i'
+                || str.charAt(str.length() - 1) == 'o' || str.charAt(str.length() - 1) == 'u' || str.charAt(str.length() - 1) == 'A'
+                || str.charAt(str.length() - 1) == 'E' || str.charAt(str.length() - 1) == 'I'
+                || str.charAt(str.length() - 1) == 'O' || str.charAt(str.length() - 1) == 'U') {
+                    check(str.substring(0, str.length()-1));
+                    count++;
         }
         return count;
     }
