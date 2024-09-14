051062088d66761949065d5852a11eb4d3b0fea1
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-14 23:15:08 +0530

    Practice Questions

--- Questions/src/Recursion/ReverseArray.java
+++ Questions/src/Recursion/ReverseArray.java
@@ -1,5 +1,13 @@
 package Recursion;
 
 public class ReverseArray {
-    
+    public void print(int arr[],int size){
+        if(size==0){
+            return;
+        }
+        else{
+            print(arr, size-1);
+            System.out.println(arr[size-1]);
+        }
+    }
 }
