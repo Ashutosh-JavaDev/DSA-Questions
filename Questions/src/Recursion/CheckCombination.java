5ad35464fc2bca5ccfee00d55fbfd5fad118f46c
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-15 10:02:52 +0530

    Practice Questions

--- Questions/src/Recursion/CheckCombination.java
+++ Questions/src/Recursion/CheckCombination.java
@@ -1,15 +1,15 @@
 package Recursion;
 
 public class CheckCombination {
-    public void check(int arr[], int size,int target) {
+    public void check(int arr[], int size, int target) {
         if (size <= 1) {
             return;
         }
-        if(arr[size-1]+arr[size-2]==target){
-            System.out.println("arr["+arr[size-1]+"]"+"arr["+arr[size-2]+"]");
-        }
-        else{
-            check(arr,size-1, target); 
+        if (arr[size - 1] + arr[size - 2] == target) {
+            System.out.println("arr[" + arr[size - 1] + "]" + "arr[" + arr[size - 2] + "]");
+            check(arr, size-1, target);
+        } else {
+            check(arr, size - 1, target);
         }
     }
 }
