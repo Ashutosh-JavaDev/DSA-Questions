ff7c249b96afb15128e9eda8a35ba305fe0f5e8a
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-15 09:54:16 +0530

    Practice Questions

--- Questions/src/Recursion/CheckCombination.java
+++ Questions/src/Recursion/CheckCombination.java
@@ -1,9 +1,10 @@
 package Recursion;
 
 public class CheckCombination {
-    public void check(int arr[],int target){
-        if(arr.length==0){
+    public void check(int arr[], int target) {
+        if (arr.length == 0) {
             return;
         }
+        
     }
 }
