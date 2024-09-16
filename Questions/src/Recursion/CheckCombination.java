cc18b325848117ac9bdddc225be7f9fb56c1286c
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-16 23:13:35 +0530

    Practice Questions

--- Questions/src/Recursion/CheckCombination.java
+++ Questions/src/Recursion/CheckCombination.java
@@ -6,7 +6,7 @@ public class CheckCombination {
             return;
         }
         if (arr[size - 1] + arr[size - 2] == target) {
-            System.out.println("arr[" + arr[size - 1] + "]" + "arr[" + arr[size - 2] + "]");
+            System.out.println("arr[" + arr[size - 1] + "]" + "+ arr[" + arr[size - 2] + "]");
             check(arr, size-1, target);
         } else {
             check(arr, size - 1, target);
