3bd5f89b9bbf36871cb4ebe19ed8c7a92aaf110d
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-20 21:48:29 +0530

    Practice Questions

--- Questions/src/Recursion/Unique.java
+++ Questions/src/Recursion/Unique.java
@@ -1,9 +1,10 @@
 package Recursion;
 
 public class Unique {
-    public void check(int arr[], int size) {
+    public void check(int arr[], int size,int index) {
         if (size == 0) {
             return;
         }
+        
     }
 }
