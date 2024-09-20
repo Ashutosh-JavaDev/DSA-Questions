8c3f82c4205faa823e443e2041066a67e544a982
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-20 21:57:33 +0530

    Practice Questions

--- Questions/src/Recursion/Unique.java
+++ Questions/src/Recursion/Unique.java
@@ -9,13 +9,18 @@ public class Unique {
         if(index==valuse){
             if(arr[size-1]==arr[index]){
                 valuse++;
-                check(arr, size-1, index);
+                check(arr, size-1, index+1);
             }
-            if(arr[size-1]!=arr[index+1]){
+            if(arr[size-1]!=arr[index]){
                 System.out.print(arr[size-1]+" ");
                 valuse++;
-                check(arr, size, index+1);
+                check(arr, size-1, index+1);
             }
         }
+    }
+    public static void main(String[]args){
+        Unique ob=new Unique();
+        int arr[]={1,2,2,3};
+        ob.check(arr, arr.length-1, 0);
     }
 }
