6498f0caf87aa82ef1b249fc5be36ee6a299590d
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-22 23:08:43 +0530

    Web dev

--- Questions/src/Recursion/StringPalindrome.java
+++ Questions/src/Recursion/StringPalindrome.java
@@ -2,10 +2,14 @@ package Recursion;
 
 public class StringPalindrome {
     public String check(String str) {
-        String st = "";
-        if(str.length()==0){
-            return "";
-        }
-        return st;
+        // String st = "";
+        // if(str.length()==0){
+        //     return "";
+        // }
+        // if(str.charAt(str.length()-1)==str.charAt(0)){
+
+        // }
+        // return st;
+        
     }
 }
