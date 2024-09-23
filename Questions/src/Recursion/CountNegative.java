dee282f5468a45fd2719f3eced77634a3d75b532
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-23 22:39:16 +0530

    Practice Question

--- Questions/src/Recursion/CountNegative.java
+++ Questions/src/Recursion/CountNegative.java
@@ -4,6 +4,9 @@ public class CountNegative {
     int Count = 0;
 
     public int count(int arr[], int size) {
+        if(size==0){
+            return 0;
+        }
         return Count;
     }
 }
