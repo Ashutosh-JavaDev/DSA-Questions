ba8573465b7869c0d330077c8805c472959db23e
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-23 22:49:11 +0530

    Practice Question

--- Questions/src/Recursion/CountNegative.java
+++ Questions/src/Recursion/CountNegative.java
@@ -7,15 +7,16 @@ public class CountNegative {
         if (size == 0) {
             return 0;
         }
-        if(arr[size-1]<0){
+        if (arr[size - 1] < 0) {
             Count++;
         }
-        count(arr, size-1);
+        count(arr, size - 1);
         return Count;
     }
-    public static void main(String[]args){
-        CountNegative ob=new CountNegative();
-        int arr[]={1,2,-2,-3,5};
-        int result=ob.count(arr, arr.length-1);
+
+    public static void main(String[] args) {
+        CountNegative ob = new CountNegative();
+        int arr[] = { 1, 2, -2, -3, 5 };
+        int result = ob.count(arr, arr.length - 1);
     }
 }
