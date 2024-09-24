b63d7b97243a738a6094f2d0df1b139dfacaad69
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-24 20:52:11 +0530

    Practice Question

--- Questions/src/Recursion/minimum.java
+++ Questions/src/Recursion/minimum.java
@@ -1,24 +1,26 @@
 package Recursion;
 
 public class minimum {
-    int arr[], size = 0, min = arr[0];
+    int min = 0;
 
-    public int CheckMinimum(int arr[]) {
-        this.arr=arr;
+    public int CheckMinimum(int arr[],int size) {
+    
         if (size == 0) {
             return 0;
         }
         if (arr[size - 1] < min) {
             min = arr[size - 1];
         }
+        CheckMinimum(arr, size-1);
         return min;
     }
-    public static void main(String[]args){
-        minimum ob=new minimum();
-        int arr[]={12,21,23,32,24};
 
-        int result=ob.CheckMinimum(arr);
-        System.out.println("Result: "+result);
+    public static void main(String[] args) {
+        minimum ob = new minimum();
+        int arr[] = { 12, 21, 23, 32, 24 };
+
+        int result = ob.CheckMinimum(arr,arr.length-1);
+        System.out.println("Result: " + result);
 
     }
 }
