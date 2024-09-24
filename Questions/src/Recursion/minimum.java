5bc5d1252c4d56ead108c90bdf3f5285446f3fc7
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-24 20:47:00 +0530

    Practice Question

--- Questions/src/Recursion/minimum.java
+++ Questions/src/Recursion/minimum.java
@@ -1,14 +1,14 @@
 package Recursion;
 
 public class minimum {
-    int arr[], size=0,min = arr[0];
+    int arr[], size = 0, min = arr[0];
 
     public int CheckMinimum() {
-        if(size==0){
+        if (size == 0) {
             return 0;
         }
-        if(arr[size-1]<min){
-            min=arr[size-1];
+        if (arr[size - 1] < min) {
+            min = arr[size - 1];
         }
         return min;
     }
