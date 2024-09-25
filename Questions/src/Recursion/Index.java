adbc77fdf18b8981338e4b6125f07f28a9e11e37
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-25 22:54:58 +0530

    Practice Question

--- Questions/src/Recursion/Index.java
+++ Questions/src/Recursion/Index.java
@@ -1,21 +1,23 @@
 package Recursion;
 
 public class Index {
-    int value=0;
-    public int Result(int arr[],int size,int target){
-        if(size==0){
+    int value = 0;
+
+    public int Result(int arr[], int size, int target) {
+        if (size == 0) {
             return 0;
         }
-        if(arr[size-1]==target){
-            value = size-1;
+        if (arr[size - 1] == target) {
+            value = size - 1;
         }
         return value;
     }
-    public static void main(String[]args){
-        Index ob=new Index();
-        int arr[]={12,21,23,43,24};
-        int result=ob.Result(arr, arr.length, 23);
 
-        System.out.println("Result: "+result);
+    public static void main(String[] args) {
+        Index ob = new Index();
+        int arr[] = { 12, 21, 23, 43, 24 };
+        int result = ob.Result(arr, arr.length, 23);
+
+        System.out.println("Result: " + result);
     }
 }
