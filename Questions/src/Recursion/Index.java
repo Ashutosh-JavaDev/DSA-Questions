de193a435d82ba0d007e820a48840c37e1c3b176
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-25 22:44:06 +0530

    Practice Question

--- Questions/src/Recursion/Index.java
+++ Questions/src/Recursion/Index.java
@@ -2,9 +2,12 @@ package Recursion;
 
 public class Index {
     int value=0;
-    public int Result(int arr[],int size){
+    public int Result(int arr[],int size,int target){
         if(size==0){
             return 0;
+        }
+        if(arr[size-1]==target){
+            
         }
         return value;
     }
