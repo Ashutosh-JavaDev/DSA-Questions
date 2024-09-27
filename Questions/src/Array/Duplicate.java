e270b71e8a521a148ee62785eaa1a3a195044289
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-27 19:16:36 +0530

    Practice Question

--- Questions/src/Array/Duplicate.java
+++ Questions/src/Array/Duplicate.java
@@ -13,6 +13,11 @@ public class Duplicate {
             for (int i = 0; i < size; i++) {
                 arr[i]=sc.nextInt();
             }
+            for(int i=0;i<size;i++){
+                for(int j=i+1;j<size;j++){
+                    
+                }
+            }
             int index=arr[0];
             return index;
         }
