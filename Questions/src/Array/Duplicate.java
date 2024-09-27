ab3dd468edfe14481f290891e5eea9ec4f91e479
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-27 19:21:56 +0530

    Practice Question

--- Questions/src/Array/Duplicate.java
+++ Questions/src/Array/Duplicate.java
@@ -2,26 +2,32 @@ package Array;
 
 import java.util.Scanner;
 
-public class Duplicate {
-    class duplicate {
-        public void Check(int arr[], int size) {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter the Size of Array");
-            size = sc.nextInt();
-            arr = new int[size];
-            System.out.println("Enter the Value in the Array");
-            for (int i = 0; i < size; i++) {
-                arr[i] = sc.nextInt();
-            }
-            for (int i = 0; i < size; i++) {
-                for (int j = i + 1; j < size; j++) {
-                    if (arr[i] == arr[j]) {
-                        System.out.print(arr[j] + " ");
-                    }
-                }
-                System.out.println();
-            }
-
+class DUplicate {
+    public void Check() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Size of Array");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        System.out.println("Enter the Value in the Array");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
         }
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.print(arr[j] + " ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
+
+public class Duplicate {
+
+    public static void main(String[] args) {
+        DUplicate ob = new DUplicate();
+        ob.Check();
     }
 }
