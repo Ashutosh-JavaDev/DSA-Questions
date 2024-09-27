f06eda570fdaf3bdf16c3a3225c45c05bae4cb0b
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-27 19:13:33 +0530

    Practice Question

--- Questions/src/Array/Duplicate.java
+++ Questions/src/Array/Duplicate.java
@@ -6,6 +6,7 @@ public class Duplicate {
     class duplicate {
         public int Check(int arr[], int size) {
             Scanner sc=new Scanner(System.in);
+            System.out.println("Enter the Size of Array");
         }
     }
 }
