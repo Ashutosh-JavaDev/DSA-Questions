32411aab7b5780f2360788f46490184d6acba3cc
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-27 19:12:59 +0530

    Practice Question

--- Questions/src/Array/Duplicate.java
+++ Questions/src/Array/Duplicate.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Duplicate {
     class duplicate {
         public int Check(int arr[], int size) {
-
+            Scanner sc=new Scanner(System.in);
         }
     }
 }
