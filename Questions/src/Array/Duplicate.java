ab636c977acc4a3d13d7f6d0cbaf85a6e3daefd7
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-27 19:18:39 +0530

    Practice Question

--- Questions/src/Array/Duplicate.java
+++ Questions/src/Array/Duplicate.java
@@ -4,22 +4,24 @@ import java.util.Scanner;
 
 public class Duplicate {
     class duplicate {
-        public int Check(int arr[], int size) {
+        public void Check(int arr[], int size) {
             Scanner sc = new Scanner(System.in);
             System.out.println("Enter the Size of Array");
             size = sc.nextInt();
             arr = new int[size];
             System.out.println("Enter the Value in the Array");
             for (int i = 0; i < size; i++) {
-                arr[i]=sc.nextInt();
+                arr[i] = sc.nextInt();
             }
-            for(int i=0;i<size;i++){
-                for(int j=i+1;j<size;j++){
-                    
+            for (int i = 0; i < size; i++) {
+                for (int j = i + 1; j < size; j++) {
+                    if(arr[i]==arr[j]){
+                        System.out.print(arr[j]+" ");
+                    }
                 }
+                System.out.println();
             }
-            int index=arr[0];
-            return index;
+            
         }
     }
 }
