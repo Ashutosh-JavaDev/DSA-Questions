a68a527f13ce724ff7217d1a5ca8cfbfb2f2041a
Ashutosh Ayush <jbnashu297@gmail.com>
2024-09-27 19:15:50 +0530

    Practice Question

--- Questions/src/Array/Duplicate.java
+++ Questions/src/Array/Duplicate.java
@@ -11,8 +11,10 @@ public class Duplicate {
             arr = new int[size];
             System.out.println("Enter the Value in the Array");
             for (int i = 0; i < size; i++) {
-
+                arr[i]=sc.nextInt();
             }
+            int index=arr[0];
+            return index;
         }
     }
 }
