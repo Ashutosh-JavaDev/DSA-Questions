e8c854d529caf4bb53cf588af5a193e164746b32
Ashutosh Ayush <jbnashu297@gmail.com>
2024-10-14 23:37:03 +0530

    Project

--- Questions/src/Array/Perfect.java
+++ Questions/src/Array/Perfect.java
@@ -6,7 +6,7 @@ class perfect{
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the Number");
         int num=sc.nextInt();
-        for(int i=1;i<=num;i++){
+        for(int i=1;i<num;i++){
             if(num%i==0){
                 sum+=i;
             }
