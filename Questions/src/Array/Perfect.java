0e0185e9f79a1b7fbb407b35cc8686d207c745c2
Ashutosh Ayush <jbnashu297@gmail.com>
2024-10-14 23:32:10 +0530

    Project

--- Questions/src/Array/Perfect.java
+++ Questions/src/Array/Perfect.java
@@ -2,7 +2,13 @@ package Array;
 import java.util.Scanner;
 class perfect{
     void check(int num){
+        int sum=0;
         Scanner sc=new Scanner(System.in);
+        for(int i=1;i<=num;i++){
+            if(num%i==0){
+                sum+=i
+            }
+        }
     }
 }
 public class Perfect {
