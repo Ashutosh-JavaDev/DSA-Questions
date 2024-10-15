66f632a126662cfd5eae45ba8d522b7fbf4aca3b
Ashutosh Ayush <jbnashu297@gmail.com>
2024-10-15 22:05:43 +0530

    Project

--- Questions/src/Loop/Prime.java
+++ Questions/src/Loop/Prime.java
@@ -2,8 +2,9 @@ package Loop;
 
 class prime {
     void Result() {
-        int count = 0;
         for (int i = 2; i <= 100; i++) {
+            int count = 0;
+
             for (int j = 1; j <= i; j++) {
                 if (i % j == 0) {
                     count++;
@@ -19,7 +20,7 @@ class prime {
 }
 
 public class Prime {
-    public static void main(String[]args) {
+    public static void main(String[] args) {
         prime ob = new prime();
         ob.Result();
     }
