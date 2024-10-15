b53cba0cbabaddb1bec2897b7719e7f670ccd011
Ashutosh Ayush <jbnashu297@gmail.com>
2024-10-15 21:48:03 +0530

    Loop

--- Questions/src/Loop/Prime.java
+++ Questions/src/Loop/Prime.java
@@ -1,21 +1,25 @@
 package Loop;
+
 import java.util.Scanner;
-class prime{
-    void Result(){
-        int count=0;
-        for(int i=2;i<=100;i++){
-            for(int j=1;j<=i;j++){
-                if(i%j==0){
+
+class prime {
+    void Result() {
+        int count = 0;
+        for (int i = 2; i <= 100; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
                     count++;
                 }
             }
-            if(count==2){
-                System.out.print(i+" ");
+            if (count == 2) {
+                System.out.print(i + " ");
             }
         }
+        System.out.println();
+
     }
-    System.out.println();
 }
+
 public class Prime {
-    
+
 }
