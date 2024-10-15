c05c3c31648051c36a76a3c58b44bf0737c9593b
Ashutosh Ayush <jbnashu297@gmail.com>
2024-10-15 21:41:59 +0530

    Project

--- Questions/src/Loop/NaturalNumber.java
+++ Questions/src/Loop/NaturalNumber.java
@@ -1,5 +1,18 @@
 package Loop;
+import java.util.Scanner;
+class naturalNumber {
+    void Print(int size) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Size the till the output want to Print");
+        size=sc.nextInt();
+        int i=1;
+        while(i<=size){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+}
 
 public class NaturalNumber {
-    
+
 }
