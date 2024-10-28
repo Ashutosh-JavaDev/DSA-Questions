67027f35857d1a23cdc4487090f0187b3f941d43
Ashutosh Ayush <jbnashu297@gmail.com>
2024-10-28 23:30:13 +0530

    HashSet

--- Questions/src/Hashset/Remove.java
+++ Questions/src/Hashset/Remove.java
@@ -1,6 +1,12 @@
 package Hashset;
 import java.util.HashSet;
 import java.util.Scanner;
+class RemoveElement{
+    public void removeElement(HashSet set){
+        set=new HashSet<>();
+        Scanner sc=new Scanner(System.in);
+    }
+}
 public class Remove {
     
 }
