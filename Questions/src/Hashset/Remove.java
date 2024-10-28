37290581f976d7c0522302f5602e068ff6994587
Ashutosh Ayush <jbnashu297@gmail.com>
2024-10-28 23:33:12 +0530

    HashSet

--- Questions/src/Hashset/Remove.java
+++ Questions/src/Hashset/Remove.java
@@ -5,6 +5,18 @@ class RemoveElement{
     public void removeElement(HashSet set){
         set=new HashSet<>();
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Value in the List\nPress Enter to Stop");
+        while(true){
+            String str=sc.nextLine();
+            if(str.equals("")){
+                break;
+            }
+            else{
+                int num=Integer.parseInt(str);
+                set.add(num);
+            }
+        }
+        System.out.println(set);
     }
 }
 public class Remove {
