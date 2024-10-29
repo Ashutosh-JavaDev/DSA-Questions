a65695d8130e1d861bba64e5fc101ae4e5d8d489
Ashutosh Ayush <jbnashu297@gmail.com>
2024-10-29 10:52:18 +0530

    HashSet

--- Questions/src/Hashset/Comapare.java
+++ Questions/src/Hashset/Comapare.java
@@ -6,6 +6,19 @@ class comapare{
     public void Comparision(){
         HashSet<Integer>set=new HashSet<>();
         HashSet<Integer>set2=new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+        set.add(5);
+
+        set2.add(1);
+        set2.add(3);
+        set2.add(2);
+        set2.add(5);
+        set2.add(4);
+
+
     }
 }
 public class Comapare {
