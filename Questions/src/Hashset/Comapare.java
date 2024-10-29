156e5bc3d4b962c7813d7cb89c315283eaced793
Ashutosh Ayush <jbnashu297@gmail.com>
2024-10-29 10:51:11 +0530

    HashSet

--- Questions/src/Hashset/Comapare.java
+++ Questions/src/Hashset/Comapare.java
@@ -1,6 +1,13 @@
 package Hashset;
 import java.util.HashSet;
 import java.util.Scanner;
+
+class comapare{
+    public void Comparision(){
+        HashSet<Integer>set=new HashSet<>();
+        HashSet<Integer>set2=new HashSet<>();
+    }
+}
 public class Comapare {
     
 }
