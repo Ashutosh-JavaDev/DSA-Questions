8fef500f0b0808d71d79439016c8b25434e3d91a
Ashutosh Ayush <jbnashu297@gmail.com>
2024-10-30 20:01:00 +0530

    HashSet

--- Questions/src/Hashset/CovertHastSet.java
+++ Questions/src/Hashset/CovertHastSet.java
@@ -1,6 +1,11 @@
 package Hashset;
 import java.util.HashSet;
 import java.util.Scanner;
+class Conversion{
+    public void convert(){
+        HashSet<Integer>set=new HashSet<>();
+    }
+}
 public class CovertHastSet {
     
 }
