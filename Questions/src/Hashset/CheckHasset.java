01a28b52c003c3f27ff3d0cd83c86d89815a46fa
Ashutosh Ayush <jbnashu297@gmail.com>
2024-11-02 21:10:30 +0530

    Hashset

--- Questions/src/Hashset/CheckHasset.java
+++ Questions/src/Hashset/CheckHasset.java
@@ -4,6 +4,8 @@ import java.util.HashSet;
 class checkSet{
     public void CheckSet(){
         HashSet<Integer>set=new HashSet<>();
+        Scanner sc=new Scanner(System.in);
+        
     }
 }
 public class CheckHasset {
