41c1f09d04a568ec9fbd21840aa48eeb5ac0c7c0
Ashutosh Ayush <jbnashu297@gmail.com>
2024-11-02 21:09:33 +0530

    Hashset

--- Questions/src/Hashset/CheckHasset.java
+++ Questions/src/Hashset/CheckHasset.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 import java.util.HashSet;
 class checkSet{
     public void CheckSet(){
-        
+        HashSet<Integer>set=new HashSet<>();
     }
 }
 public class CheckHasset {
