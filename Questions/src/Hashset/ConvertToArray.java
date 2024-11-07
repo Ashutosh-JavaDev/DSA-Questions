9e5149ccd40c0bf957cbc4bdea12b90d9bab4f13
Ashutosh Ayush <jbnashu297@gmail.com>
2024-11-07 22:01:55 +0530

    Convert to Array

--- Questions/src/Hashset/ConvertToArray.java
+++ Questions/src/Hashset/ConvertToArray.java
@@ -6,34 +6,33 @@ import java.util.Scanner;
 class convertArray {
     public void ArrayConversion() {
         HashSet<Integer> set = new HashSet<>();
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the  Value in the list\nPress Enter to Stop");
         while (true) {
-            String str=sc.nextLine();
-            if(str.equals("")){
+            String str = sc.nextLine();
+            if (str.equals("")) {
                 break;
-            }
-            else{
-                int num=Integer.parseInt(str);
+            } else {
+                int num = Integer.parseInt(str);
                 set.add(num);
             }
         }
         System.out.println(set);
-        int arr[] = new int[set.size()]; 
-       int i=0;
-       for(int val:set){
-        arr[i++]=val;
-       }
-       for(int n:arr){
-       System.out.println(n); 
-       }
+        int arr[] = new int[set.size()];
+        int i = 0;
+        for (int val : set) {
+            arr[i++] = val;
+        }
+        for (int n : arr) {
+            System.out.println(n);
+        }
 
     }
 }
 
 public class ConvertToArray {
-    public static void main(String[]args){
-        convertArray ob=new convertArray();
+    public static void main(String[] args) {
+        convertArray ob = new convertArray();
         ob.ArrayConversion();
     }
 }
