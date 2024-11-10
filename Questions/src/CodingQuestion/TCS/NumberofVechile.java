bc78175e63fa45394f4984c800bdf419f3942ba7
Ashutosh Ayush <jbnashu297@gmail.com>
2024-11-10 22:44:40 +0530

    TCS

--- Questions/src/CodingQuestion/TCS/NumberofVechile.java
+++ Questions/src/CodingQuestion/TCS/NumberofVechile.java
@@ -1,5 +1,9 @@
 package CodingQuestion.TCS;
-
+import java.util.Scanner;
+class countVechile{
+    public int countVechile(int twoVechicle,int fourVechile){
+        
+}
 public class NumberofVechile {
     
 }
