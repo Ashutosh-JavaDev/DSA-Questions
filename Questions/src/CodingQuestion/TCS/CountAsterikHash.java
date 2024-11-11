a4272f61713d912d91d54238c316d19aa9db0253
Ashutosh Ayush <jbnashu297@gmail.com>
2024-11-11 21:28:45 +0530

    TCS

--- Questions/src/CodingQuestion/TCS/CountAsterikHash.java
+++ Questions/src/CodingQuestion/TCS/CountAsterikHash.java
@@ -6,7 +6,7 @@ class countAsterikHash{
         String str="#**##*";
         for(int i=0;i<str.length();i++){
             if(str.charAt(i)=='*'){
-                
+                count++;
             }
         }
     }
