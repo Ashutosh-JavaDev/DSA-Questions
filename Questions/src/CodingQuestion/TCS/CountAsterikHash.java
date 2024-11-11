c2f524877140812a8a9395ded5331551c50b931e
Ashutosh Ayush <jbnashu297@gmail.com>
2024-11-11 21:23:37 +0530

    TCS

--- Questions/src/CodingQuestion/TCS/CountAsterikHash.java
+++ Questions/src/CodingQuestion/TCS/CountAsterikHash.java
@@ -1,5 +1,11 @@
 package CodingQuestion.TCS;
 import java.util.Scanner;
+class countAsterikHash{
+    public void countAsterikHash(){
+        int count=0;
+        int count2=0;
+    }
+}
 public class CountAsterikHash {
     
 }
