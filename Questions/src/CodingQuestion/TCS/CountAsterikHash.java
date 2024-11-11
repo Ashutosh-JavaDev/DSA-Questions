b6cacd8d891a53be476724cb66016b7062fd39cf
Ashutosh Ayush <jbnashu297@gmail.com>
2024-11-11 21:24:23 +0530

    TCS

--- Questions/src/CodingQuestion/TCS/CountAsterikHash.java
+++ Questions/src/CodingQuestion/TCS/CountAsterikHash.java
@@ -4,6 +4,8 @@ class countAsterikHash{
     public void countAsterikHash(){
         int count=0;
         int count2=0;
+        Scanner sc=new Scanner(System.in);
+        
     }
 }
 public class CountAsterikHash {
