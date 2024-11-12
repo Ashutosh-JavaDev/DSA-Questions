1430d2293d12f3d4e7d6a45bd267b1c4fb20a57d
Ashutosh Ayush <jbnashu297@gmail.com>
2024-11-12 22:48:20 +0530

    C_Program

--- Questions/src/C_Program/PerfectNumber.c
+++ Questions/src/C_Program/PerfectNumber.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 void main (){
-    int num,i,j,sum;
+    int num,i,j,sum,d;
     printf("Enter the Number till the Value want to Find\n");
     scanf("%d",&num);
     for(i=1;i<=num;i++){
@@ -8,11 +8,13 @@ void main (){
         for(j=1;j<i;j++){
             if(i%j==0){
                 sum+=j;
+                d+=sum;
             }
         }
         if(sum==i){
             printf("%d ",i);
         }
     }
+    printf("Sum of Perfet Number : %d",d);
     printf("\n");
 }
