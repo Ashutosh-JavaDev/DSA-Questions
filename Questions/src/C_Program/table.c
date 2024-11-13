4f61582df8d8a9e038e3a26c47e356cb63984f2c
Ashutosh Ayush <jbnashu297@gmail.com>
2024-11-13 21:58:04 +0530

    C_Program

--- Questions/src/C_Program/table.c
+++ Questions/src/C_Program/table.c
@@ -2,11 +2,11 @@
 int main()
 {
     int a = 2;
-    int *ptr;
-    int i,v;
+    int *ptr,*v;
+    int i;
     ptr = &a;
     for(i=1;i<=10;i++){
-        v=*ptr * i;
-        printf(v," ");
+        *v=*ptr * i;
+        printf("%d ",v);
     }
 }
