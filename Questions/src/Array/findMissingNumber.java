7fe9b37fe047d8f8c4eecbbbd889b4e3fa4f24fa
Ashutosh Ayush <jbnashu297@gmail.com>
2024-11-15 13:38:59 +0530

    DSA

--- Questions/src/Array/findMissingNumber.java
+++ Questions/src/Array/findMissingNumber.java
@@ -11,5 +11,17 @@ class MissingNumber{
     }
 }
 public class findMissingNumber {
-    
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the Value in the Array");
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        MissingNumber ob=new MissingNumber();
+        int res=ob.findNumber(arr, size);
+        System.out.println("Result: "+res);
+    }
 }
