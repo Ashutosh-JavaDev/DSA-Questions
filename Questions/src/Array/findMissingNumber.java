7c8ab4df908eb066bbb23a567e048f81a4015e61
Ashutosh Ayush <jbnashu297@gmail.com>
2024-11-15 13:35:27 +0530

    DSA

--- Questions/src/Array/findMissingNumber.java
+++ Questions/src/Array/findMissingNumber.java
@@ -1,12 +1,13 @@
 package Array;
 import java.util.Scanner;
 class MissingNumber{
-    public void findNumber(int arr[],int size){
+    public int findNumber(int arr[],int size){
         int n=arr.length+1;
         int sum=(n*(n+1))/2;
         for(int i=0;i<size;i++){
-            
+            sum-=arr[i];
         }
+        return sum;
     }
 }
 public class findMissingNumber {
