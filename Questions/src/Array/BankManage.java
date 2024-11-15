764c579332558e017dac44700ab59d60d30dfb85
Ashutosh Ayush <jbnashu297@gmail.com>
2024-11-15 17:14:57 +0530

    DSA

---
+++ Questions/src/Array/BankManage.java
@@ -0,0 +1,35 @@
+package Array;
+
+class Bankcheck {
+    private int amount;
+    private int CreditMoney;
+    private int DebitMoney;
+
+
+    public int read(int amount) {
+        this.amount = amount;
+        return amount;
+    }
+    public int credit(int addMoney){
+        CreditMoney=amount+addMoney;
+        amount=CreditMoney;
+        return CreditMoney;
+    }
+    public int debit(int subMoney){
+       while(true){
+        if(subMoney>amount){
+            System.out.println("Insufficent Balance: "+amount);
+            continue;
+        }
+        else{
+            DebitMoney=amount-subMoney;
+            return DebitMoney;
+        }
+       }
+
+    }
+}
+
+public class BankManage {
+
+}
