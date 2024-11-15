3a95adecb4d3a8b08e8a68dd4d971fbbc95f5e82
Ashutosh Ayush <jbnashu297@gmail.com>
2024-11-15 13:32:52 +0530

    DSA

--- Questions/src/Array/findMissingNumber.java
+++ Questions/src/Array/findMissingNumber.java
@@ -1,7 +1,9 @@
 package Array;
 import java.util.Scanner;
 class MissingNumber{
-    
+    public void findNumber(int arr[]){
+        int n=arr.length+1;
+    }
 }
 public class findMissingNumber {
     
