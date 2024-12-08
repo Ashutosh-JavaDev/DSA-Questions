3c95e2dd14f8de23fc5ee7df86fa0ff3f25c8250
Ashutosh Ayush <jbnashu297@gmail.com>
2024-12-09 00:25:24 +0530

    Program

--- Questions/src/C_Program/SumOfDigits.c
+++ Questions/src/C_Program/SumOfDigits.c
@@ -1,17 +1,16 @@
 #include<stdio.h>
-int sum=0;
-int sOd(int num){
+int sOd(int num,int sum){
     if(num<0){
         return 0;
     }
     sum+=num%10;
-    sOd(num/10);
+    sOd(num/10,sum);
     return sum;
 }
 void main(){
     int num;
     printf("Enter the Number\n");
     scanf("%d",&num);
-    int res=sOd(num);
+    int res=sOd(num,0);
     printf("Result: %d",res);
 }
