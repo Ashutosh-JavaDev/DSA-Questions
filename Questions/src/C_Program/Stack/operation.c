0f6fffc9440728f898c559e42ab841ba054ffab9
Ashutosh Ayush <jbnashu297@gmail.com>
2024-12-10 22:50:14 +0530

    Program

--- Questions/src/C_Program/Stack/operation.c
+++ Questions/src/C_Program/Stack/operation.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 
-
-#define MAX 100  
+#define MAX 100
 typedef struct
 {
     int size;
     int top;
     int arr[];
-}Stack;
-void initilization(Stack *s){
-    s->top=-1
+} Stack;
+void initilization(Stack *s)
+{
+    s->top = -1;
 }
-int isFull(Stack s){
-    return s.top==MAX-1;
+int isFull(Stack s)
+{
+    return s.top == MAX - 1;
 }
-void push(struct stack,int arr[]){
-    if(isFull(stack)){
-        
+void push(struct stack, int arr[])
+{
+    if (isFull(stack))
+    {
     }
 }
 void main()
