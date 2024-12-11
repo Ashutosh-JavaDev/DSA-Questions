a1332e8704c09dc0d513c87e702aa0c8b126036b
Ashutosh Ayush <jbnashu297@gmail.com>
2024-12-11 22:40:43 +0530

    Program

--- Questions/src/Recursion/Fibonacci.java
+++ Questions/src/Recursion/Fibonacci.java
@@ -1,13 +1,13 @@
 package Recursion;
 class fibo{
-    void series(int num,int n,int m,int sum){
+    void series(int num,int n,int m){
         if(num==0){
             return;
         }
-        sum+=n;
+        n+=m;
         System.out.print(n+" ");
 
-        series(num-1, m, sum,sum);
+        series(num-1, m, n);
     }
 }
 public class Fibonacci {
