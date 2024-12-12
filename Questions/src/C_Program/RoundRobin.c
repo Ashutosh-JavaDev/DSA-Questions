e38553840853902720fd2406fcf9f4d4a8bc5f04
Ashutosh Ayush <jbnashu297@gmail.com>
2024-12-12 21:51:37 +0530

    RoundRobin

--- Questions/src/C_Program/RoundRobin.c
+++ Questions/src/C_Program/RoundRobin.c
@@ -2,8 +2,11 @@
 #include<stdbool.h>
 
 void roundRobinScheduling(int processes[],int n,int brustTime[],int quantum){
-    int waitingTime[n],turnaroundTime[n],remainingTime[n];
+    int waitingTime[n],turnaroundTime[n],remainingbrustTime[n];
     int totalWaitingTime=0,totalWaitingTime=0;
 
     //Initialize remaining burst Times
+    for(int i=0;i<n;i++){
+        remainingbrustTime[i]=brustTime[i];
+    }
 }
