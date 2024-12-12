6927845dcfde669520991ddea3d2eb6620461ad5
Ashutosh Ayush <jbnashu297@gmail.com>
2024-12-12 21:48:49 +0530

    RoundRobin

--- Questions/src/C_Program/RoundRobin.c
+++ Questions/src/C_Program/RoundRobin.c
@@ -3,4 +3,7 @@
 
 void roundRobinScheduling(int processes[],int n,int brustTime[],int quantum){
     int waitingTime[n],turnaroundTime[n],remainingTime[n];
+    int totalWaitingTime=0,totalWaitingTime=0;
+
+    //Initialize remaining burst Times
 }
