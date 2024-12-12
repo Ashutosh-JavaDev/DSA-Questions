711905087294902137097da935041493e5c026db
Ashutosh Ayush <jbnashu297@gmail.com>
2024-12-12 21:56:23 +0530

    RoundRobin

--- Questions/src/C_Program/RoundRobin.c
+++ Questions/src/C_Program/RoundRobin.c
@@ -1,34 +1,44 @@
-#include<stdio.h>
-#include<stdbool.h>
+#include <stdio.h>
+#include <stdbool.h>
 
-void roundRobinScheduling(int processes[],int n,int brustTime[],int quantum){
-    int waitingTime[n],turnaroundTime[n],remainingbrustTime[n];
-    int totalWaitingTime=0,totalWaitingTime=0;
+void roundRobinScheduling(int processes[], int n, int brustTime[], int quantum)
+{
+    int waitingTime[n], turnaroundTime[n], remainingbrustTime[n];
+    int totalWaitingTime = 0, totalWaitingTime = 0;
 
-    //Initialize remaining burst Times
-    for(int i=0;i<n;i++){
-        remainingbrustTime[i]=brustTime[i];
+    // Initialize remaining burst Times
+    for (int i = 0; i < n; i++)
+    {
+        remainingbrustTime[i] = brustTime[i];
     }
-    int time=0;
+    int time = 0;
 
-    while(true){
-        bool allDone=true;
-        for(int i=0;i<n;i++){
-            if(remainingbrustTime[i]>0){
-                allDone=false;
+    while (true)
+    {
+        bool allDone = true;
+        for (int i = 0; i < n; i++)
+        {
+            if (remainingbrustTime[i] > 0)
+            {
+                allDone = false;
 
-                if(remainingbrustTime[i]>quantum){
-                    time+=quantum;
-                    remainingbrustTime[i]-=quantum;
+                if (remainingbrustTime[i] > quantum)
+                {
+                    time += quantum;
+                    remainingbrustTime[i] -= quantum;
                 }
-                else{
-                    //Process Finishes Execution
-                    time+=remainingbrustTime[i];
-                    waitingTime[i]=time-brustTime[i];
+                else
+                {
+                    // Process Finishes Execution
+                    time += remainingbrustTime[i];
+                    waitingTime[i] = time - brustTime[i];
                     // waitingTime[i]=time-brustTime[i];
-                    remainingbrustTime[i]=0;
+                    remainingbrustTime[i] = 0;
                 }
             }
         }
+
+        if (allDone)
+            break;
     }
 }
