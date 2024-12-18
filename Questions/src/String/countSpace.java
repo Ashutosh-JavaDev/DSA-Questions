9596213b03c08d48be444f1d9fbd22326cab0d17
Ashutosh Ayush <jbnashu297@gmail.com>
2024-12-18 23:54:30 +0530

    String

--- Questions/src/String/countSpace.java
+++ Questions/src/String/countSpace.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 class StringCount{
     void Count(String str){
     Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the String");
+    str=sc.nextLine();
+    
     }
 }
 public class countSpace {
