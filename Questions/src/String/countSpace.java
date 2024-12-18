15a74236f47592d3a00d513b37c23a66622eb10f
Ashutosh Ayush <jbnashu297@gmail.com>
2024-12-18 23:57:30 +0530

    String

--- Questions/src/String/countSpace.java
+++ Questions/src/String/countSpace.java
@@ -1,11 +1,16 @@
 package String;
 import java.util.Scanner;
 class StringCount{
-    void Count(String str){
+    int Count(String str){
+        int count=0;
     Scanner sc=new Scanner(System.in);
     System.out.println("Enter the String");
     str=sc.nextLine();
-    
+    for(int i=0;i<str.length();i++){
+        if(str.charAt(i)==' '){
+            count++;
+        }
+    }
     }
 }
 public class countSpace {
