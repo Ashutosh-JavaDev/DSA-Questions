aaf8273d228e83ee0f81ca71e1a79e8451be38dc
Ashutosh Ayush <jbnashu297@gmail.com>
2024-12-19 23:48:42 +0530

    Array

--- Questions/src/Array/Majority.java
+++ Questions/src/Array/Majority.java
@@ -5,7 +5,9 @@ class majorityElement{
         int num=0,count=0;
         for(int i=0;i<size;i++){
             for(int j=i+1;j<size;j++){
-                
+                if(arr[i]==arr[j]){
+                    
+                }
             }
         }
         return num;
