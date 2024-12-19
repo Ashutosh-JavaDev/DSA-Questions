81c73a6b18262d81f874ea527718fcbdc8013668
Ashutosh Ayush <jbnashu297@gmail.com>
2024-12-19 23:46:37 +0530

    Array

--- Questions/src/Array/Majority.java
+++ Questions/src/Array/Majority.java
@@ -2,8 +2,12 @@ package Array;
 import java.util.Scanner;
 class majorityElement{
     static int element(int arr[],int size){
-        int num=0;
-        
+        int num=0,count=0;
+        for(int i=0;i<size;i++){
+            for(int j=i+1;j<size;j++){
+                
+            }
+        }
         return num;
     }
 }
