c1d9a0a31ea86efbb718d8aae2b6a25a6754a583
Ashutosh Ayush <jbnashu297@gmail.com>
2024-12-21 16:57:34 +0530

    Array

--- Questions/src/Array/Majority.java
+++ Questions/src/Array/Majority.java
@@ -19,5 +19,9 @@ public class Majority {
         System.out.println("Enter the size of the Array");
         int size=sc.nextInt();
         int arr[]=new int[size];
+        System.out.println("Enter the value in the Array");
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
     }
 }
