4be68131ae9962becf3c365fd3bd1a89116b422c
Ashutosh Ayush <jbnashu297@gmail.com>
2024-12-21 16:55:54 +0530

    Array

--- Questions/src/Array/Majority.java
+++ Questions/src/Array/Majority.java
@@ -16,5 +16,8 @@ class majorityElement{
 public class Majority {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the Array");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
     }
 }
