2057cd70fe2a89e319f5d7c376605a331f06a963
Ashutosh Ayush <jbnashu297@gmail.com>
2024-12-22 18:34:39 +0530

    Array

--- Questions/src/Array/SecondLargest.java
+++ Questions/src/Array/SecondLargest.java
@@ -3,8 +3,14 @@ class secondLargest{
      int Result(int arr[],int size){
         int max=arr[0];
         int secMax=arr[0];
-        
-        return secMax;
+        for(int i=0;i<size;i++){
+            for(int j=i+1;j<size;j++){
+                if(arr[i]>arr[j]){
+                    max=arr[i];
+                }
+            }
+        }
+        return max;
      }
 }
 public class SecondLargest {
