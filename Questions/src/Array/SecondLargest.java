7ec6d8f0d2a87926c6167f6fd4970b652bebe340
Ashutosh Ayush <jbnashu297@gmail.com>
2024-12-22 18:22:00 +0530

    Array

--- Questions/src/Array/SecondLargest.java
+++ Questions/src/Array/SecondLargest.java
@@ -1,7 +1,9 @@
 package Array;
 class secondLargest{
      int Result(int arr[],int size){
-        
+        int max=arr[0];
+        int secMax=arr[0];
+        return secMax;
      }
 }
 public class SecondLargest {
