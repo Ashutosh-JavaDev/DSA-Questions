d6082c54fbfbd59bc7138f1b01c22b913433c9e2
Ashutosh Ayush <jbnashu297@gmail.com>
2024-12-23 21:18:25 +0530

    Array

--- Questions/src/Array/SecondLargest.java
+++ Questions/src/Array/SecondLargest.java
@@ -1,37 +1,34 @@
 package Array;
-
 import java.util.Scanner;
-
-class secondLargest {
-    int Result(int arr[], int size) {
-        int max = arr[0];
-        int secMax = arr[0];
-        for (int i = 0; i < size; i++) {
-
-            if (arr[i] > max) {
-                max = arr[i];
-                if (secMax < max && secMax > arr[i]) {
-                    secMax = arr[i];
+class secondLargest{
+     int Result(int arr[],int size){
+        int max=arr[0];
+        int secMax=arr[0];
+        for(int i=0;i<size;i++){
+            
+                if(arr[i]>max){
+                    max=arr[i];
+                    if(secMax<max&&secMax>arr[i]){
+                        secMax=arr[i];
+                    }
                 }
-
-            }
-            return secMax;
+            
         }
-    }
-
-    public class SecondLargest {
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter the Size of the Array");
-            int size = sc.nextInt();
-            int arr[] = new int[size];
-            System.out.println("Enter the Value in the Array");
-            for (int i = 0; i < size; i++) {
-                arr[i] = sc.nextInt();
-            }
-            secondLargest ob = new secondLargest();
-            int result = ob.Result(arr, size);
-            System.out.println("Maximum Number: " + result);
+        return secMax;
+     }
+}
+public class SecondLargest {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Size of the Array");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the Value in the Array");
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
         }
+        secondLargest ob=new secondLargest();
+        int result=ob.Result(arr, size);
+        System.out.println("Maximum Number: "+result);
     }
 }
