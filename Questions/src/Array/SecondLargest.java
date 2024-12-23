6dc0e18d30252be6a6a12a9299b4552b69dc72d0
Ashutosh Ayush <jbnashu297@gmail.com>
2024-12-23 21:23:31 +0530

    Array

--- Questions/src/Array/SecondLargest.java
+++ Questions/src/Array/SecondLargest.java
@@ -1,32 +1,36 @@
 package Array;
+
 import java.util.Scanner;
-class secondLargest{
-     int Result(int arr[],int size){
-        int max=arr[0];
-        int secMax=arr[0];
-        for(int i=0;i<size;i++){
-            
-                if(arr[i]>max){
-                    secMax=arr[i];
-                   max=arr[i];
-                }
-            
+
+class secondLargest {
+    int Result(int arr[], int size) {
+        int max = arr[0];
+        int secMax = arr[0];
+        for (int i = 0; i < size; i++) {
+
+            if (arr[i] > max) {
+                secMax = arr[i];
+                max = arr[i];
+            }
+
         }
+        System.out.println("Maximum Number: "+max);
         return secMax;
-     }
+    }
 }
+
 public class SecondLargest {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Size of the Array");
-        int size=sc.nextInt();
-        int arr[]=new int[size];
+        int size = sc.nextInt();
+        int arr[] = new int[size];
         System.out.println("Enter the Value in the Array");
-        for(int i=0;i<size;i++){
-            arr[i]=sc.nextInt();
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
         }
-        secondLargest ob=new secondLargest();
-        int result=ob.Result(arr, size);
-        System.out.println("Second Maximum Number: "+result);
+        secondLargest ob = new secondLargest();
+        int result = ob.Result(arr, size);
+        System.out.println("Second Maximum Number: " + result);
     }
 }
