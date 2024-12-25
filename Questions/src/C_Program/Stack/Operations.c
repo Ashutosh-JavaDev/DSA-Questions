84eee6068c8ff9efc8998c9ae8325626dde808e6
Ashutosh Ayush <jbnashu297@gmail.com>
2024-12-25 15:02:56 +0530

    Stack Implementation

--- Questions/src/C_Program/Stack/Operations.c
+++ Questions/src/C_Program/Stack/Operations.c
@@ -3,4 +3,22 @@
 void main(){
     int choice;
     printf("1.to Push\n2.to Pop\n3.to Peek\n4.Exit");
+    scanf("%d",&choice);
+    switch(choice){
+        case 1:
+        push();
+        break;
+        case 2:
+        pop();
+        break;
+        case 3:
+        peek();
+        break;
+        case 4:
+        exit(0);
+        break;
+        default:
+        printf("Press Valid Button\n");
+        break;
+    }
 }
