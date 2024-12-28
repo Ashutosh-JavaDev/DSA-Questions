a2b6955783f6e9d4e23f9cea7a0e988f095c4352
Ashutosh Ayush <jbnashu297@gmail.com>
2024-12-28 12:05:21 +0530

    Stack Implementation

--- Questions/src/Array/ZeroatEnd.java
+++ Questions/src/Array/ZeroatEnd.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 class setZeroAtEnd {
     void result(int arr[], int size) {
-        int count=0;
+        int count = 0;
         for (int i = 0; i < size; i++) {
             if (arr[i] != 0) {
 
                 System.out.print(arr[i] + " ");
                 count++;
             }
-           
+
         }
         System.out.println();
     }
@@ -22,12 +22,12 @@ public class ZeroatEnd {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the Array");
         int size = sc.nextInt();
-        int arr[]=new int[size];
+        int arr[] = new int[size];
         System.out.println("Enter the value in the Array");
-        for(int i=0;i<size;i++){
-            arr[i]=sc.nextInt();
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
         }
-        setZeroAtEnd ob=new setZeroAtEnd();
+        setZeroAtEnd ob = new setZeroAtEnd();
         ob.result(arr, size);
     }
 }
