f4b2c5b87450fc517f21f5b9abd0bb20dac5ea01
Ashutosh Ayush <jbnashu297@gmail.com>
2024-12-28 11:57:56 +0530

    Stack Implementation

--- Questions/src/Array/ZeroatEnd.java
+++ Questions/src/Array/ZeroatEnd.java
@@ -17,6 +17,10 @@ public class ZeroatEnd {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the Array");
         int size = sc.nextInt();
-
+        int arr[]=new int[size];
+        System.out.println("Enter the value in the Array");
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
     }
 }
