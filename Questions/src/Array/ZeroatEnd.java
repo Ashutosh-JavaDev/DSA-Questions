830f634e1695dcb24bc955ff0a9b48382041b3bb
Ashutosh Ayush <jbnashu297@gmail.com>
2024-12-28 11:55:10 +0530

    Stack Implementation

--- Questions/src/Array/ZeroatEnd.java
+++ Questions/src/Array/ZeroatEnd.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 class setZeroAtEnd {
     void result(int arr[], int size) {
         for(int i=0;i<size;i++){
-            
+            if(arr[i]!=0){
+                System.out.print(arr[i]+" ");
+            }
         }
     }
 }
