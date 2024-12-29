8652ebcbcb710e350f543c3b2d15b0215d749b8b
Ashutosh Ayush <jbnashu297@gmail.com>
2024-12-29 21:36:19 +0530

    Stack Implementation

--- Questions/src/Array/Reverse.java
+++ Questions/src/Array/Reverse.java
@@ -1,5 +1,15 @@
 package Array;
-
+class reverse{
+    void result(int arr[],int size){
+        if(0<size){
+            return;
+        }
+        else{
+            result(arr, size-1);
+            System.out.println(arr[size]);
+        }
+    }
+}
 public class Reverse {
     
 }
