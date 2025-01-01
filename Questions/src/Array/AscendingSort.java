d7e601b9fc693c64bb2e6ecd12ba7d941614cd92
Ashutosh Ayush <jbnashu297@gmail.com>
2025-01-02 00:04:06 +0530

    Stack Implementation

--- Questions/src/Array/AscendingSort.java
+++ Questions/src/Array/AscendingSort.java
@@ -2,7 +2,15 @@ package Array;
 
 class Sorting{
     void sorting(int arr[]){
-        
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[i]>arr[j]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
     }
 }
 public class AscendingSort {
