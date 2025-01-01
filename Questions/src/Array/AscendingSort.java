520d7dcfed2ef34be5051073a24e88f8e1f9ca39
Ashutosh Ayush <jbnashu297@gmail.com>
2025-01-02 00:06:38 +0530

    Stack Implementation

--- Questions/src/Array/AscendingSort.java
+++ Questions/src/Array/AscendingSort.java
@@ -4,7 +4,7 @@ class Sorting{
     void sorting(int arr[]){
         for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr.length-i-1;j++){
-                if(arr[i]>arr[j]){
+                if(arr[i]<arr[j]){
                     int temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
@@ -13,7 +13,7 @@ class Sorting{
         }
         System.out.println("Array After sort in Ascending Order");
         for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]);
+            System.out.print(arr[i]+" ");
         }
     }
 
