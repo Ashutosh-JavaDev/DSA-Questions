f3be68c5232e26d5701ca3a80b398374098e302e
Ashutosh Ayush <jbnashu297@gmail.com>
2025-01-03 22:15:24 +0530

    Stack Implementation

--- Questions/src/Array/Roated.java
+++ Questions/src/Array/Roated.java
@@ -3,6 +3,7 @@ package Array;
 class rotated {
     void result(int arr[],int size,int rotate){
         int temp[],j=0;
+        temp=new int[rotate];
         for(int i=0;i<rotate;i++){
             temp[j]=arr[i];
            j++;
