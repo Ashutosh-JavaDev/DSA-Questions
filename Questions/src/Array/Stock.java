8eac8472be9ea34d068ffc52fac1e4d0c5563b18
Ashutosh Ayush <jbnashu297@gmail.com>
2025-01-05 10:11:53 +0530

    Stack Implementation

--- Questions/src/Array/Stock.java
+++ Questions/src/Array/Stock.java
@@ -2,13 +2,22 @@ package Array;
 
 import java.util.Scanner;
 
-class stock {
-    int maximum(int arr[], int size, int buy, int sell) {
-        int sum = 0;
-        return sum;
-    }
-}
+// class stock {
+//     int maximum(int arr[], int size, int buy, int sell) {
+//         int sum = 0;
+//         while(1>0){
+
+//         }
+//         return sum;
+//     }
+// }
 
 public class Stock {
-
+    public static void main(String[] args) {
+        int i=1;
+        while(1>0){
+            System.out.print(i+" ");
+            i++;
+        }
+    }
 }
