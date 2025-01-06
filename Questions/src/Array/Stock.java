2faf136651ed13aa488884de523e439e0ec933d2
Ashutosh Ayush <jbnashu297@gmail.com>
2025-01-06 23:18:32 +0530

    DSA

--- Questions/src/Array/Stock.java
+++ Questions/src/Array/Stock.java
@@ -2,22 +2,22 @@ package Array;
 
 import java.util.Scanner;
 
-// class stock {
-//     int maximum(int arr[], int size, int buy, int sell) {
-//         int sum = 0;
-//         while(1>0){
+class stock {
+    int maximum(int arr[], int size, int buy, int sell) {
+        int sum = 0;
+        while(1>0){
 
-//         }
-//         return sum;
-//     }
-// }
+        }
+        return sum;
+    }
+}
 
 public class Stock {
     public static void main(String[] args) {
         int i=1;
         while(1>0){
             System.out.print(i+" ");
-            i++;
+            
         }
     }
 }
