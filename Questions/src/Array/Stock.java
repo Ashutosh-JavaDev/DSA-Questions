0bab5a432109a2b19182e4a7a78ec26853a08944
Ashutosh Ayush <jbnashu297@gmail.com>
2025-01-07 21:57:21 +0530

    DSA

--- Questions/src/Array/Stock.java
+++ Questions/src/Array/Stock.java
@@ -6,15 +6,15 @@ class stock {
     int maximum(int arr[], int size, int buy, int sell) {
         Scanner sc=new Scanner(System.in);
         int sum = 0;
-        // while(1>0){
-        //     System.out.println("Enter the Price at which you want to buy the Stock");
-        //     buy=sc.nextInt();
-        //     // System.out.println("Enter the price at which you want to sell the Stock");
-        //     // sell=sc.nextInt();
-        //     // for(int i=0;i<size;i++){
-        //     //     if(arr[i])
-        //     // }
-        // }
+        while(1>0){
+            System.out.println("Enter the Price at which you want to buy the Stock");
+            buy=sc.nextInt();
+            // System.out.println("Enter the price at which you want to sell the Stock");
+            // sell=sc.nextInt();
+            for(int i=0;i<size;i++){
+                if(arr[i])
+            }
+        }
         return sum;
     }
 }
