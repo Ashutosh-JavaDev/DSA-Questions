cfa129c95dac2a6ac245169294843874d5911348
Ashutosh Ayush <jbnashu297@gmail.com>
2025-01-12 23:35:35 +0530

    Stack File

--- Questions/src/Array/Stock.java
+++ Questions/src/Array/Stock.java
@@ -6,17 +6,17 @@ class stock {
     int maximum(int arr[], int size, int buy, int sell) {
         Scanner sc=new Scanner(System.in);
         int sum = 0;
-        while(1>0){
-            System.out.println("Enter the Price at which you want to buy the Stock");
-            buy=sc.nextInt();
-            // System.out.println("Enter the price at which you want to sell the Stock");
-            // sell=sc.nextInt();
-            for(int i=0;i<size;i++){
-                if(arr[i]==size){
+        // while(1>0){
+        //     System.out.println("Enter the Price at which you want to buy the Stock");
+        //     buy=sc.nextInt();
+        //     // System.out.println("Enter the price at which you want to sell the Stock");
+        //     // sell=sc.nextInt();
+        //     for(int i=0;i<size;i++){
+        //         if(arr[i]==size){
                     
-                }
-            }
-        }
+        //         }
+        //     }
+        // }
         return sum;
     }
 }
