0ac8b47fa52347768ed0aa842757a26e919e0ed6
Ashutosh Ayush <jbnashu297@gmail.com>
2025-01-16 22:11:12 +0530

    Stack File

--- Questions/src/C_Program/Stack/AscendingOrder.c
+++ Questions/src/C_Program/Stack/AscendingOrder.c
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 #define MAX 10
 int arr[MAX];
+int top=-1;
 void push(){
     int data;
-    if
+    if(top==MAX-1){
+        printf("Stack OverFlow\n");
+    }
 }
 void pop();
 void peek();
