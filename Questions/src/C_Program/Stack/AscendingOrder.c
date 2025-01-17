e864174b8c9cc6dbb7f9d08fcd753b53e50ce332
Ashutosh Ayush <jbnashu297@gmail.com>
2025-01-18 00:10:37 +0530

    Stack File

--- Questions/src/C_Program/Stack/AscendingOrder.c
+++ Questions/src/C_Program/Stack/AscendingOrder.c
@@ -2,30 +2,37 @@
 #include <stdlib.h>
 #define MAX 10
 int arr[MAX];
-int top=-1;
-void push(){
+int top = -1;
+void push()
+{
     int data;
-    if(top==MAX-1){
+    if (top == MAX - 1)
+    {
         printf("Stack OverFlow\n");
     }
-    else{
+    else
+    {
         printf("Enter the value to add in the stack\n");
-        scanf("%d",&data);
-        top=top+1;
-        arr[top]=data;
-        printf("Value in %d is %d \n",top,arr[top]);
+        scanf("%d", &data);
+        top = top + 1;
+        arr[top] = data;
+        printf("Value in %d is %d \n", top, arr[top]);
     }
 }
 void pop();
 void peek();
-void ascending(){
-
+void ascending()
+{
 }
 void main()
 {
     int choice;
-    switch(choice){
-    printf("Enter the choice wisely\n1.To Push\n2.To Pop\n3.To Peek\n4.To print Ascending\n5.To print Descending\n5.To EXIT");
-    scanf("%d",&choice);
-    }    
+    while (1)
+    {
+        switch (choice)
+        {
+            printf("Enter the choice wisely\n1.To Push\n2.To Pop\n3.To Peek\n4.To print Ascending\n5.To print Descending\n5.To EXIT");
+            scanf("%d", &choice);
+        }
+    }
 }
