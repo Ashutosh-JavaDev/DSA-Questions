7823dc043758d73c2a58e186113e04b15382ab93
Ashutosh Ayush <jbnashu297@gmail.com>
2025-01-27 23:04:05 +0530

    Queue

--- Questions/src/C_Program/Stack/CircularQueue.c
+++ Questions/src/C_Program/Stack/CircularQueue.c
@@ -2,3 +2,37 @@
 #define MAX 5
 int front=-1;
 int rear=-1;
+
+void front();
+void rear();
+void peek();
+void display();
+int main()
+{
+    int choice;
+    while (1)
+    {
+        printf("1.To Enqueue()\n2.To Dequeue()\n3.Peek()\n4.To Display()\n");
+        scanf("%d", &choice);
+        printf("----------------------------------------------------\n");
+        switch (choice)
+        {
+        case 1:
+            Enqueue();
+            break;
+
+        case 2:
+            Dequeue();
+            break;
+        case 3:
+            Peek();
+            break;
+        case 4:
+            Display();
+            break;
+        default:
+            printf("Choose Wisely\n");
+            break;
+        }
+    }
+}
