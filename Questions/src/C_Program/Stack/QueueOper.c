2a4574b47f2b45a78a0d46a26fafeeabf5c34180
Ashutosh Ayush <jbnashu297@gmail.com>
2025-01-27 21:29:53 +0530

    Queue

--- Questions/src/C_Program/Stack/QueueOper.c
+++ Questions/src/C_Program/Stack/QueueOper.c
@@ -1,13 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 
 #define MAX 5
 int queue[MAX];
-int front =-1;
-int rear=-1;
+int front = -1;
+int rear = -1;
 void Display();
 void Peek();
 void Dequeue();
 void Enqueue();
-int main(){
-
+int main()
+{
 }
