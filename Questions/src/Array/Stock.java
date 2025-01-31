c32b468c8ed54bab06f272fc8b700a4048a05a48
Ashutosh Ayush <jbnashu297@gmail.com>
2025-02-01 00:21:26 +0530

    Stock

--- Questions/src/Array/Stock.java
+++ Questions/src/Array/Stock.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 class stock {
     int maximum(int arr[], int size, int buy, int sell) {
-        // Scanner sc=new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
         int sum = 0;
  
         return sum;
