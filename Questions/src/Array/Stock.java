c95163801ac590b83ab23fc98c7a3e57b2dc44a2
Ashutosh Ayush <jbnashu297@gmail.com>
2025-01-31 23:58:19 +0530

    Stock

--- Questions/src/Array/Stock.java
+++ Questions/src/Array/Stock.java
@@ -14,9 +14,6 @@ class stock {
 public class Stock {
     public static void main(String[] args) {
         int i=1;
-        // while(1>0){
-        //     System.out.print(i+" ");
-            
-        // }
+     
     }
 }
