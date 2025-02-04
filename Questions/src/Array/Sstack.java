9751ea8fc6a6bb9a39ac20777fc43067e4b6327f
Ashutosh Ayush <jbnashu297@gmail.com>
2025-02-04 23:53:00 +0530

    Stack

--- Questions/src/Array/Sstack.java
+++ Questions/src/Array/Sstack.java
@@ -1,13 +1,16 @@
 package Array;
 class stack{
+    int size=10;
+    int arr[]=new int[size];
     void push(){
+        int data;
 
     }
     void pop(){
 
     }
     void peek(){
-        
+
     }
 }
 public class Sstack {
