8424072a4329a870531d2977943c3b9ea1abb119
Ashutosh Ayush <jbnashu297@gmail.com>
2025-02-04 23:53:54 +0530

    Stack

--- Questions/src/Array/Sstack.java
+++ Questions/src/Array/Sstack.java
@@ -2,9 +2,12 @@ package Array;
 class stack{
     int size=10;
     int arr[]=new int[size];
+    int top=-1;
     void push(){
         int data;
-
+        if(top==size-1){
+            System.out.println("Stack Overflow");
+        }
     }
     void pop(){
 
