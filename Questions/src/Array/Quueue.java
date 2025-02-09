289093531ccfce5ae9760f146ea0adccbfb27a5d
Ashutosh Ayush <jbnashu297@gmail.com>
2025-02-10 00:12:32 +0530

    queue

--- Questions/src/Array/Quueue.java
+++ Questions/src/Array/Quueue.java
@@ -2,7 +2,7 @@ package Array;
 import java.util.Scanner;
 class quueue{
     int size=10;
-int arr[]=new arr[size];
+// int arr[]=new arr[size];
 }
 public class Quueue {
  public static void main(String[]args){
