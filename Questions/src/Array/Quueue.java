bedfab61963887db1c50ae75ac373c5abe32863f
Ashutosh Ayush <jbnashu297@gmail.com>
2025-02-27 21:28:42 +0530

    tic and toc

--- Questions/src/Array/Quueue.java
+++ Questions/src/Array/Quueue.java
@@ -1,11 +1,19 @@
 package Array;
-import java.util.Scanner;
-class quueue{
-    int size=10;
-// int arr[]=new arr[size];
-}
-public class Quueue {
- public static void main(String[]args){
 
- }   
+import java.util.Scanner;
+
+class quueue {
+    Scanner sc=new Scanner(System.in);
+    int size = 10;
+    int top=-1;
+    int arr[] = new int[size];
+    void Enqueue(){
+
+    }
+}
+
+public class Quueue {
+    public static void main(String[] args) {
+
+    }
 }
