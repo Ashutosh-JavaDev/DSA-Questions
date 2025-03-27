67098b4083a6ae99d91f0599d8ab69f9fa8d7e96
Ashutosh Ayush <jbnashu297@gmail.com>
2025-03-27 23:06:09 +0530

    Java Push

--- Questions/src/Array/LinkedListOperation.java
+++ Questions/src/Array/LinkedListOperation.java
@@ -3,17 +3,24 @@ package Array;
 class Node {
     int data;
     Node next;
-    Node (int data){
-        this.data=data;
+
+    Node(int data) {
+        this.data = data;
+    }
+
+    void Display() {
+
     }
 }
-class operation{
-    public int InsertAtBeginning(int data){
-        int temp=0;
+
+class operation {
+    public int InsertAtBeginning(int data) {
+        int temp = 0;
         return temp;
 
     }
 }
+
 public class LinkedListOperation {
 
 }
