6852e09c446a0bc516ce8f6f52d3ee3afbd50c23
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-17 20:50:54 +0530

    Stack by ArrayList

---
+++ Questions/src/Array/StackbyArrayList.java
@@ -0,0 +1,10 @@
+package Array;
+import java.util.Stack;
+public class StackbyArrayList {
+    public static void main(String[] args) {
+        Stack sc=new Stack<>();
+        sc.push(12);
+        sc.peek();
+        System.out.println(sc);
+    }
+}
