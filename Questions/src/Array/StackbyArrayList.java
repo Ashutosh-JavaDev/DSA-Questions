a06ac65adb4770090adce217953b8ca710df06af
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-17 21:02:33 +0530

    Stack by ArrayList

--- Questions/src/Array/StackbyArrayList.java
+++ Questions/src/Array/StackbyArrayList.java
@@ -1,13 +1,18 @@
 package Array;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 class stackImplementation {
     Scanner sc = new Scanner(System.in);
     private int data;
-
+    ArrayList stack=new ArrayList<Integer>();
+    int top=-1;
     void push(int data) {
         this.data = data;
+        if(top==stack.size()-1){
+            System.out.println("Stack OverFlow");
+        }
     }
 }
 
