ed64e40435690c757cb08ae070a857b58aeaac64
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-17 21:14:26 +0530

    Stack Using ArrayList

--- Questions/src/Array/StackbyArrayList.java
+++ Questions/src/Array/StackbyArrayList.java
@@ -4,20 +4,25 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 class stackImplementation {
-    Scanner sc = new Scanner(System.in);
     private int data;
-    ArrayList stack=new ArrayList<Integer>();
-    int top=-1;
+    ArrayList stack = new ArrayList<Integer>();
+    int top = -1;
+
     void push(int data) {
         this.data = data;
-        if(top==stack.size()-1){
+        if (top == stack.size() - 1) {
             System.out.println("Stack OverFlow");
+        } else {
+            top=top+1;
+            stack.add(data);
+            System.out.println(data+" is added at "+top+" index");
         }
     }
 }
 
 public class StackbyArrayList {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
     }
 }
