3e4707ab55f6d877ca2950412dcf04fb0f761a19
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-17 20:54:27 +0530

    Stack by ArrayList

--- Questions/src/Array/StackbyArrayList.java
+++ Questions/src/Array/StackbyArrayList.java
@@ -1,10 +1,18 @@
 package Array;
-import java.util.Stack;
+
+import java.util.Scanner;
+
+class stackImplementation {
+    Scanner sc = new Scanner(System.in);
+    private int data;
+
+    void push(int data) {
+        this.data = data;
+    }
+}
+
 public class StackbyArrayList {
     public static void main(String[] args) {
-        Stack sc=new Stack<>();
-        sc.push(12);
-        sc.peek();
-        System.out.println(sc);
+
     }
 }
