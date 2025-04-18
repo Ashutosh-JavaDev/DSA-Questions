320d94e0db4680d9f11b6861a704ddec758d6c39
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-18 11:04:43 +0530

    Stack by ArrayList

--- Questions/src/Array/StackbyArrayList.java
+++ Questions/src/Array/StackbyArrayList.java
@@ -5,9 +5,14 @@ import java.util.ArrayList;
 
 class stackImplementation {
     private int data;
-    ArrayList stack = new ArrayList<Integer>();
+    ArrayList<Integer> stack = new ArrayList<>();
     int top = -1;
-
+    boolean isEmpty(){
+        if(top==-1){
+            return true;
+        }
+        return false;
+    }
     void push(int data) {
         this.data = data;
         if (top == stack.size() - 1) {
@@ -16,6 +21,14 @@ class stackImplementation {
             top=top+1;
             stack.add(data);
             System.out.println(data+" is added at "+top+" index");
+        }
+    }
+    void pop(){
+        if(top==-1){
+            System.out.println("Stack UnderFlow,Push Data First");
+        }
+        else{
+            
         }
     }
 }
