301f3baaf6ffa03abdf2da9f4f8023aa56052d9e
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-19 23:08:16 +0530

    Stack by ArrayList

--- Questions/src/Array/StackbyLinkedList.java
+++ Questions/src/Array/StackbyLinkedList.java
@@ -1,5 +1,13 @@
 package Array;
-
+class Nodes{
+    int data;
+    Nodes next;
+    Nodes(int data){
+        this.data=data;
+    }
+}
 public class StackbyLinkedList {
-    
+    public static void main(String[] args) {
+        
+    }
 }
