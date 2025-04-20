c118f5f5c87b052f08f2f6111a8cad25287dc77d
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-21 00:37:38 +0530

    Recursion Problem

--- Questions/src/Recursion/StringApperance.java
+++ Questions/src/Recursion/StringApperance.java
@@ -1,13 +1,15 @@
-class StringFun{
-    void ApperanceString(String str,int first,int last){
-    if(last>str.length()-1){
-        return;
-    }
+class StringFun {
+    void ApperanceString(String str, int first, int last) {
+        if (last > str.length() - 1) {
+            return;
+        } else {
 
+        }
     }
 }
+
 public class StringApperance {
     public static void main(String[] args) {
-        
+
     }
 }
