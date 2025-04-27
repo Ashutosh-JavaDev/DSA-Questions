31de94bda8ebf4914df50bd33c9630c13edf9701
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-27 23:52:40 +0530

    Creating Hashset Function

--- Questions/src/Hashset/AllOperation.java
+++ Questions/src/Hashset/AllOperation.java
@@ -1,9 +1,14 @@
 package Hashset;
 import java.util.HashSet;
+import java.util.Scanner;
 class Operations{
     public static void functions(){
-        HashSet set=new HashSet<>();
+        HashSet<String> set=new HashSet<>();
+        Scanner sc=new Scanner(System.in);
         System.out.println("Enter the value want to add in set\nPress stop to STOP");
+        while(true){
+            String str=sc.nextLine();
+        }
     }
 }
 public class AllOperation {
