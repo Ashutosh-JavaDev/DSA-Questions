d741d28387ae13222866aa012ce98d32e3b99744
Ashutosh Ayush <jbnashu297@gmail.com>
2025-05-02 11:26:45 +0530

    HashSet Program

--- Questions/src/Hashset/UniqueWordAnalyzer.java/main.java
+++ Questions/src/Hashset/UniqueWordAnalyzer.java/main.java
@@ -3,12 +3,21 @@
 import java.util.HashSet;
 import java.util.Scanner;
 import javax.swing.*;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class main extends JFrame implements ActionListener {
+    JTextField field;
+    JButton button;
      public main(){
-
+        setLocation(450, 100);
+        setTitle("REGISTRATION FORM");
+        setLayout(null);
+        setSize(800, 850);
+        getContentPane().setBackground(Color.white);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
      }
      public void actionPerformed(ActionEvent ae) {
         setVisible(false);
