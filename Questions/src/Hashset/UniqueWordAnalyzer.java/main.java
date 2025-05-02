158f6e847feaf5c13a0c4ef6a5b051119b859b45
Ashutosh Ayush <jbnashu297@gmail.com>
2025-05-02 11:48:47 +0530

    Java Hashset Project

--- Questions/src/Hashset/UniqueWordAnalyzer.java/main.java
+++ Questions/src/Hashset/UniqueWordAnalyzer.java/main.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Scanner;
 import javax.swing.*;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -11,6 +12,14 @@ public class main extends JFrame implements ActionListener {
     JTextField field;
     JButton button;
      public main(){
+        JLabel l1=new JLabel("Enter the Paragraph:");
+        l1.setBounds(200,100,250,30);
+        l1.setFont(new Font("Arial", Font.BOLD, 20));
+        add(l1);
+        field.setBounds(150,150,250,250);
+        field.setFont(new Font("Arial",Font.PLAIN,10));
+        add(field);
+        // All Important Configuration
         setLocation(550, 100);
         setTitle("UNIQUE WORD ANALYZER");
         setLayout(null);
