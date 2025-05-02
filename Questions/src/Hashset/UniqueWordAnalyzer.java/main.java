ae499d0f5f5bfbd86a70e6527708d635f037a6b8
Ashutosh Ayush <jbnashu297@gmail.com>
2025-05-02 12:01:09 +0530

    Java Hashset Project

--- Questions/src/Hashset/UniqueWordAnalyzer.java/main.java
+++ Questions/src/Hashset/UniqueWordAnalyzer.java/main.java
@@ -3,22 +3,26 @@
 import java.util.HashSet;
 import java.util.Scanner;
 import javax.swing.*;
+
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class main extends JFrame implements ActionListener {
-    JTextField field;
+    JTextArea field;
     JButton button;
      public main(){
         JLabel l1=new JLabel("Enter the Paragraph:");
         l1.setBounds(200,100,250,30);
         l1.setFont(new Font("Arial", Font.BOLD, 20));
         add(l1);
-        field=new JTextField();
-        field.setBounds(150,150,250,250);
-        field.setFont(new Font("Arial",Font.PLAIN,10));
+        field=new JTextArea();
+        field.setBounds(130,150,350,250);
+        field.setFont(new Font("Arial",Font.PLAIN,15));
+        field.setLineWrap(true); // Enable word wrapping
+        field.setWrapStyleWord(true); 
         add(field);
         // All Important Configuration
         setLocation(550, 100);
