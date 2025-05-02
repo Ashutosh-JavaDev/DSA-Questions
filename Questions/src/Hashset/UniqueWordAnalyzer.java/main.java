e6cb8b00113cf8a129cf5de50991fd81767161e0
Ashutosh Ayush <jbnashu297@gmail.com>
2025-05-02 11:23:33 +0530

    HashSet Program

--- Questions/src/Hashset/UniqueWordAnalyzer.java/main.java
+++ Questions/src/Hashset/UniqueWordAnalyzer.java/main.java
@@ -1,6 +1,16 @@
+
 // package Hashset.UniqueWordAnalyzer;
 import java.util.HashSet;
 import java.util.Scanner;
-public class main {
-    
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class main extends JFrame implements ActionListener {
+     public main(){
+
+     }
+     public void actionPerformed(ActionEvent ae) {
+        setVisible(false);
+    }
 }
