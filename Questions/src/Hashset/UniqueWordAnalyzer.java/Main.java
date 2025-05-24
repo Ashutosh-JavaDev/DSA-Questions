6bfb55fc2e83f40d9adcf7bef54f7c665c7e2471
Ashutosh Ayush <jbnashu297@gmail.com>
2025-05-25 00:24:56 +0530

    Hastset Created

--- Questions/src/Hashset/UniqueWordAnalyzer.java/Main.java
+++ Questions/src/Hashset/UniqueWordAnalyzer.java/Main.java
@@ -10,20 +10,29 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class main extends JFrame implements ActionListener {
+public class Main extends JFrame implements ActionListener {
     JTextArea field;
     JButton button;
-     public main(){
+     public Main(){
         JLabel l1=new JLabel("Enter the Paragraph:");
         l1.setBounds(200,100,250,30);
         l1.setFont(new Font("Arial", Font.BOLD, 20));
         add(l1);
+        // Text Area
         field=new JTextArea();
         field.setBounds(130,150,350,250);
         field.setFont(new Font("Arial",Font.PLAIN,15));
         field.setLineWrap(true); // Enable word wrapping
         field.setWrapStyleWord(true); 
         add(field);
+        // Button
+        button=new JButton("Sumbit");
+        button.setBounds(100, 550, 400, 50);
+        button.setForeground(Color.white);
+        button.setBackground(Color.red);
+        button.setFont(new Font("Arial",Font.PLAIN,15));
+        add(button);
+        button.addActionListener(this);
         // All Important Configuration
         setLocation(550, 100);
         setTitle("UNIQUE WORD ANALYZER");
@@ -34,9 +43,12 @@ public class main extends JFrame implements ActionListener {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
      }
      public void actionPerformed(ActionEvent ae) {
+        if(ae.getSource()==button){
+            HashSet <String>  set=new HashSet<>();
+        }
         setVisible(false);
     }
     public static void main(String[] args) {
-        main ob=new main();
+        new Main();
     }
 }
