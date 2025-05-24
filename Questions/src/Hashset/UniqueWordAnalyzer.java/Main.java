51ce66f21c61a82333346db6e472cb4d727bf98a
Ashutosh Ayush <jbnashu297@gmail.com>
2025-05-25 00:53:07 +0530

    Hastset Created

--- Questions/src/Hashset/UniqueWordAnalyzer.java/Main.java
+++ Questions/src/Hashset/UniqueWordAnalyzer.java/Main.java
@@ -13,50 +13,53 @@ import java.awt.event.ActionListener;
 public class Main extends JFrame implements ActionListener {
     JTextArea field;
     JButton button;
-     public Main(){
-        JLabel l1=new JLabel("Enter the Paragraph:");
-        l1.setBounds(200,100,250,30);
+
+    public Main() {
+        JLabel l1 = new JLabel("Enter the Paragraph:");
+        l1.setBounds(200, 100, 250, 30);
         l1.setFont(new Font("Arial", Font.BOLD, 20));
         add(l1);
         // Text Area
-        field=new JTextArea();
-        field.setBounds(130,150,350,250);
-        field.setFont(new Font("Arial",Font.PLAIN,15));
+        field = new JTextArea();
+        field.setBounds(130, 150, 350, 250);
+        field.setFont(new Font("Arial", Font.PLAIN, 15));
         field.setLineWrap(true); // Enable word wrapping
-        field.setWrapStyleWord(true); 
+        field.setWrapStyleWord(true);
         add(field);
         // Button
-        button=new JButton("Sumbit");
+        button = new JButton("Sumbit");
         button.setBounds(100, 550, 400, 50);
         button.setForeground(Color.white);
         button.setBackground(Color.red);
-        button.setFont(new Font("Arial",Font.PLAIN,15));
+        button.setFont(new Font("Arial", Font.PLAIN, 15));
         add(button);
         button.addActionListener(this);
         // All Important Configuration
         setLocation(550, 100);
         setTitle("UNIQUE WORD ANALYZER");
         setLayout(null);
-        setSize(600,650);
+        setSize(600, 650);
         getContentPane().setBackground(Color.white);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-     }
-     public void actionPerformed(ActionEvent ae) {
-        if(ae.getSource()==button){
-            HashSet <String>  set=new HashSet<>();
-            HashSet <String> duplicate=new HashSet<>();
-            String str=field.getText();
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == button) {
+            HashSet<String> set = new HashSet<>();
+            HashSet<String> duplicate = new HashSet<>();
+            String str = field.getText();
             set.add(str);
-            for(int i=0;i<set.size();i++){
-                for(int j=i+1;j<set.size();j++){
-                    
+            for (int i = 0; i < set.size(); i++) {
+                for (int j = i + 1; j < set.size(); j++) {
+
                 }
             }
-                
+
         }
         setVisible(false);
     }
+
     public static void main(String[] args) {
         new Main();
     }
