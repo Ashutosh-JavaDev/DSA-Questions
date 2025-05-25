bdc9a730c4c40242a7f18e4448be9c6a6525927f
Ashutosh Ayush <jbnashu297@gmail.com>
2025-05-26 00:31:23 +0530

    Tree Created

--- Questions/src/Tree/Insert.java
+++ Questions/src/Tree/Insert.java
@@ -8,21 +8,28 @@ class Node {
     Node(int data) {
         this.data = data;
     }
-    public Node insertInNull(Node root ,int val){
-        if(root==null){
-             root=new Node(val);
+
+    public Node insertInNull(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
             return root;
         }
-        if(root.data>val){
-            root.left=insertInNull(root.left, val);
-        }
-        else{
-            root.Right=insertInNull(root.Right, val);
+        if (root.data > val) {
+            root.left = insertInNull(root.left, val);
+        } else {
+            root.Right = insertInNull(root.Right, val);
         }
         return root;
     }
 }
 
 public class Insert {
-
+    public static void main(String[] args) {
+        Node newNode = new Node(0);
+        int arr[] = { 3, 1, 4, 2, 5, 4 };
+        Node root = null;
+        for (int i =0;i<arr.length;i++) {
+            newNode.insertInNull(root, arr[i]);
+        }
+    }
 }
