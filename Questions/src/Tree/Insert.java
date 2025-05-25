3acd0d9de6c293b0d288ddd3855f871c8cbd8268
Ashutosh Ayush <jbnashu297@gmail.com>
2025-05-26 00:16:32 +0530

    Tree Created

--- Questions/src/Tree/Insert.java
+++ Questions/src/Tree/Insert.java
@@ -1,12 +1,22 @@
 package Tree;
-class Node{
+
+class Node {
+    Node root;
     Node left;
     Node Right;
     int data;
-    Node(int data){
-        this.data=data;
+
+    Node(int data) {
+        this.data = data;
+    }
+    public void insertInNull(int val){
+        if(root==null){
+            Node newNode=new Node(val);
+            root=newNode;
+        }
     }
 }
+
 public class Insert {
-    
+
 }
