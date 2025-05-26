e41fbfc7a695797d78a782fa56ae5d73555fb9db
Ashutosh Ayush <jbnashu297@gmail.com>
2025-05-26 23:18:30 +0530

    Binary search tree

--- Questions/src/Tree/SearchBst.java
+++ Questions/src/Tree/SearchBst.java
@@ -6,7 +6,19 @@ class Node{
     Node(int data){
         this.data=data;
     }
-    
+    public static Node insertion(Node root,int val){
+        if(root==null){
+            return root;
+        }
+        else if(root.data>val){
+            root.Left=insertion(root.Left, val);
+        }
+        else{
+            root.Right=insertion(root.Right, val);
+        }
+        return root;
+    }
+
 }
 public class SearchBst {
     
