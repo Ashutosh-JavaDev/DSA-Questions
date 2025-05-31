3c3d66681843c43702d6b2f19dd1b2b6e54610f9
Ashutosh Ayush <jbnashu297@gmail.com>
2025-05-31 18:29:52 +0530

    Java MIN MAX value

---
+++ Questions/src/Hashset/GFG/IntegerMINandMAX.java
@@ -0,0 +1,13 @@
+package Hashset.GFG;
+
+public class IntegerMINandMAX {
+    public static int minMax(){
+        int arr[]={12,32,214,42,13};
+        int max=Integer.MAX_VALUE;
+        return max;
+    }
+    public static void main(String[]args){
+        int res=minMax();
+        System.out.println(res);
+    }
+}
