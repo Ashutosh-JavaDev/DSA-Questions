c98485d85c183c96857a617d0687772bfd226675
Ashutosh Ayush <jbnashu297@gmail.com>
2025-05-31 18:54:21 +0530

    Java Array Reverse

--- Questions/src/Hashset/GFG/ReverseArray.java
+++ Questions/src/Hashset/GFG/ReverseArray.java
@@ -1,5 +1,21 @@
 package Hashset.GFG;
 
 public class ReverseArray {
-    
+    public static void Rarray(){
+        int arr[]={21,12,24,342,132};
+        int start=0,end=arr.length-1;
+        while(start<end){
+            int temp=start;
+            start=end;
+            end=temp;
+            start++;
+            end--;
+        }
+        for(int num:arr){
+            System.out.print(num+" ");
+        }
+    }
+    public static void main(String[]args){
+        Rarray();
+    }
 }
