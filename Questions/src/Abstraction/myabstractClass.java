cad394a2053712102eb059c3544ffe3e4b04dc2f
Ashutosh Ayush <jbnashu297@gmail.com>
2025-06-14 23:17:18 +0530

    Abstraction performance

--- Questions/src/Abstraction/myabstractClass.java
+++ Questions/src/Abstraction/myabstractClass.java
@@ -1,14 +1,26 @@
 package Abstraction;
 abstract class MyabstractClass{
     abstract void greet();
+    abstract void sound();
 }
 class inheritAbstractClass extends MyabstractClass{
     @Override
     void greet() {
         System.out.println("Good Morning");
+    }
+    void sound(){
         
     }
 }
+class abst extends inheritAbstractClass{
+    @Override
+    void greet(){
+        System.out.println("Good Afternoon");
+    }
+}
 public class myabstractClass {
+    inheritAbstractClass ob=new inheritAbstractClass();
     
+    // ob.greet();
+    // ob.sound();
 }
