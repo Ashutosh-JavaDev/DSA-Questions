4180b060375487f530968cf59d4cd6f058ca0a7c
Ashutosh Ayush <jbnashu297@gmail.com>
2025-06-19 23:38:24 +0530

    Catch Exception

--- Questions/src/ExceptionHandling/CatchingException.java
+++ Questions/src/ExceptionHandling/CatchingException.java
@@ -1,5 +1,17 @@
 package ExceptionHandling;
 
+class ExceptionCatch {
+    void CatchMeathod() {
+        try {
+            int a = 10, b = 20;
+            int c = a / b;
+            System.out.println("The value of c is: " + c);
+        } catch (ArithmeticException e) {
+            System.out.println("Arithmetic Exception caught: " + e.getMessage());
+        }
+    }
+}
+
 public class CatchingException {
-    
+
 }
