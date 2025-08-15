af85024dfa72f15a296b9dd29703a4bbfa4e7cc8
Ashutosh Ayush <jbnashu297@gmail.com>
2025-08-15 12:33:08 +0530

    Abstraction

--- Questions/src/OOPS/Abstraction/Interface.java
+++ Questions/src/OOPS/Abstraction/Interface.java
@@ -2,5 +2,10 @@ package OOPS.Abstraction;
 
 abstract class Interface {
     abstract String payment();
-    
+}
+class Rayzor{
+    public String pay(){
+        String payment="Razor payment Successful";
+        return payment;
+    }
 }
