b44feb247c7abea83491d67f801a58f8bd938fe3
Ashutosh Ayush <jbnashu297@gmail.com>
2025-08-26 00:39:22 +0530

    Java Sub Array

---
+++ Questions/src/Array/MAXSubArray.java
@@ -0,0 +1,12 @@
+package Array;
+
+public class MAXSubArray {
+    public static int Sum(int[] arr){
+        int sum=0;
+        int maxSum=0;
+        for(int i=0;i<arr.length;i++){
+            
+        }
+        return maxSum;
+    }
+}
