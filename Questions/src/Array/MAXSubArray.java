fc7f695b717ec9ef93706da9d871ce10663e5c9b
Ashutosh Ayush <jbnashu297@gmail.com>
2025-08-26 00:46:28 +0530

    Java Frame added

--- Questions/src/Array/MAXSubArray.java
+++ Questions/src/Array/MAXSubArray.java
@@ -17,6 +17,7 @@ public class MAXSubArray {
         System.out.println("Enter the size of the Array");
         int size=sc.nextInt();
         int arr[]=new int[size];
+        System,out.println("Enter the value in the Array");
         for(int i=0;i<size;i++){
             arr[i]=sc.nextInt();
         }
