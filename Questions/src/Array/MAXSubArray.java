407851aef6a9af2d546dbbc2d26d805f3e4b208a
Ashutosh Ayush <jbnashu297@gmail.com>
2025-08-26 00:41:15 +0530

    Java Sub Array

--- Questions/src/Array/MAXSubArray.java
+++ Questions/src/Array/MAXSubArray.java
@@ -2,10 +2,11 @@ package Array;
 
 public class MAXSubArray {
     public static int Sum(int[] arr){
-        int sum=0;
-        int maxSum=0;
+        int sum=arr[0];
+        int maxSum=arr[0];
         for(int i=0;i<arr.length;i++){
-            
+            sum=Math.max(arr[i], sum);
+            maxSum=Math.max(maxSum, sum);
         }
         return maxSum;
     }
