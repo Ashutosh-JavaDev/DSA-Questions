8604a86b3c36e1af13fa5c35a5152a33115ee4f1
Ashutosh Ayush <jbnashu297@gmail.com>
2026-02-27 20:09:59 +0530

    Duplicate values

--- Questions/src/Array/DuplicateValue.java
+++ Questions/src/Array/DuplicateValue.java
@@ -1,14 +1,8 @@
 package Array;
-
+import java.util.HashSet;
 public class DuplicateValue {
     public static void main(String[] args) {
-        int arr[]={1,1,2,3,3,4,5,4};
-        for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]==arr[j]){
-                    System.out.print(arr[i]+" ");
-                }
-            }
-        }
+      HashSet<Integer>set=new HashSet<>();
+      
     }
 }
