5f7f9005f2880d1fb7b8250491cc09bd0a2fd056
Ashutosh Ayush <jbnashu297@gmail.com>
2026-02-27 20:12:34 +0530

    Duplicate values

--- Questions/src/Array/DuplicateValue.java
+++ Questions/src/Array/DuplicateValue.java
@@ -1,8 +1,12 @@
 package Array;
 import java.util.HashSet;
+import java.util.Scanner;
 public class DuplicateValue {
     public static void main(String[] args) {
       HashSet<Integer>set=new HashSet<>();
-      
+      Scanner sc=new Scanner(System.in);
+      while(true){
+        set.add(sc.nextInt());
+      }
     }
 }
