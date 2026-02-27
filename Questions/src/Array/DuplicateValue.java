e9b8ab445d1cb1a81903f8f2b54ac08a1624e582
Ashutosh Ayush <jbnashu297@gmail.com>
2026-02-27 20:24:50 +0530

    Duplicate values

--- Questions/src/Array/DuplicateValue.java
+++ Questions/src/Array/DuplicateValue.java
@@ -1,12 +1,27 @@
 package Array;
+
+import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Scanner;
+
 public class DuplicateValue {
     public static void main(String[] args) {
-      HashSet<Integer>set=new HashSet<>();
-      Scanner sc=new Scanner(System.in);
-      while(true){
-        set.add(sc.nextInt());
-      }
+        ArrayList<Integer>list=new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(2);
+        list.add(3);
+        list.add(2);
+        list.add(1);
+        list.add(4);
+        list.add(2);
+        HashSet<Integer> dupli = new HashSet<>();
+        for (int val : list) {
+            if (!set.add(val)) {
+                dupli.add(val);
+            }
+        }
+        System.out.println(dupli);
     }
 }
