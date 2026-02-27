02bfd92a861ff263e5e4df0d5217952ef49b37d4
Ashutosh Ayush <jbnashu297@gmail.com>
2026-02-27 20:07:22 +0530

    Duplicate values

---
+++ Questions/src/Array/DuplicateValue.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class DuplicateValue {
+    public static void main(String[] args) {
+        int arr[]={1,1,2,3,3,4,5,4};
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]==arr[j]){
+                    System.out.print(arr[i]+" ");
+                }
+            }
+        }
+    }
+}
